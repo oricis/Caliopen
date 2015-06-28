3cd2db0a0269a1c993df7c9c1f9d38a3c55ecea3
Aymeric Barantal <mric@gandi.net>
2015-06-28 10:35:00 +0200

    configure authorization policy

--- caliopen/api/user/config.py
+++ caliopen/api/user/config.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 import logging
 
-from .authentication import AuthenticationPolicy
+from .authentication import AuthenticationPolicy, AuthorizationPolicy
 
 log = logging.getLogger(__name__)
 
@@ -11,6 +11,9 @@ log = logging.getLogger(__name__)
 def includeme(config):
     """Configure REST API for user and contact."""
     config.set_authentication_policy(AuthenticationPolicy())
+    config.set_authorization_policy(AuthorizationPolicy())
+    log.debug('Loading user API')
+    config.scan('caliopen.api.user.user')
 
     log.debug('Loading contact API')
     config.scan('caliopen.api.user.contact')
