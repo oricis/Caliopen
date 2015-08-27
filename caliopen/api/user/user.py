9e699cf1aff291bdd1f7dae6fdc1dd5e4aa2f1f9
Aymeric Barantal <mric@gandi.net>
2015-08-27 16:46:35 +0200

    add an API to get user informations

--- caliopen/api/user/user.py
+++ caliopen/api/user/user.py
@@ -4,13 +4,15 @@ from __future__ import unicode_literals
 import logging
 
 from pyramid.security import NO_PERMISSION_REQUIRED
-from pyramid.httpexceptions import HTTPBadRequest
+from pyramid.httpexceptions import HTTPBadRequest, HTTPUnauthorized
 from cornice.resource import resource, view
 from caliopen.api.base.context import DefaultContext
 from .util import create_token
 
 from caliopen.base.user.core import User
 from caliopen.api.base import Api, make_url
+
+from caliopen.base.user.returns import ReturnUser
 
 log = logging.getLogger(__name__)
 
@@ -54,3 +56,19 @@ class AuthenticationAPI(Api):
         return {'user_id': user.user_id,
                 'username': user.name,
                 'tokens': tokens}
+
+
+@resource(path=make_url('/users/{user_id}'),
+          name='User',
+          factory=DefaultContext)
+class UserAPI(Api):
+
+    """User API."""
+
+    @view(renderer='json', permission='authenticated')
+    def get(self):
+        user_id = self.request.matchdict.get('user_id')
+        if user_id != self.request.authenticated_userid.user_id:
+            raise HTTPUnauthorized()
+        user = User.get(user_id)
+        return ReturnUser.build(user).serialize()
