630106ea61f63d4e005344bc3771485d162f86ff
Aymeric Barantal <mric@gandi.net>
2015-10-24 13:24:27 +0200

    a simple route for user creation

--- caliopen/api/user/user.py
+++ caliopen/api/user/user.py
@@ -6,7 +6,9 @@ import datetime
 
 import colander
 from pyramid.security import NO_PERMISSION_REQUIRED
+from pyramid.httpexceptions import HTTPCreated
 from cornice.resource import resource, view
+
 from caliopen.api.base.context import DefaultContext
 from .util import create_token
 
@@ -14,6 +16,7 @@ from caliopen.base.user.core import User
 from caliopen.api.base import Api
 from caliopen.api.base.exception import AuthenticationError
 
+from caliopen.base.user.parameters import NewUser
 from caliopen.base.user.returns import ReturnUser
 from caliopen.base.exception import CredentialException
 
@@ -79,18 +82,51 @@ class UserGetParameter(colander.MappingSchema):
     user_id = colander.SchemaNode(colander.String(), location='path')
 
 
+class PostUserPararameter(colander.MappingSchema):
+
+  """Parameter to create a new user."""
+
+  username = colander.SchemaNode(colander.String(), location='body')
+  password = colander.SchemaNode(colander.String(), location='body')
+
+
+class PostUserReturn(colander.MappingSchema):
+
+    """Return parameter upon user creation."""
+
+    location = colander.SchemaNode(colander.String(encoding='utf-8'),
+                                   location='header')
+    status_code = colander.SchemaNode(colander.Integer(),
+                                      location='status_code',
+                                      default=HTTPCreated.code)
+
+
 @resource(path='/users/{user_id}',
+          collection_path='/users',
           name='User',
-          factory=DefaultContext,
-          schema=UserGetParameter)
+          factory=DefaultContext)
 class UserAPI(Api):
 
     """User API."""
 
-    @view(renderer='json', permission='authenticated')
+    @view(renderer='json',
+          permission='authenticated',
+          schema=UserGetParameter)
     def get(self):
         user_id = self.request.validated['user_id']
         if user_id != self.request.authenticated_userid.user_id:
             raise AuthenticationError()
         user = User.get(user_id)
         return ReturnUser.build(user).serialize()
+
+    @view(renderer='json',
+          permission=NO_PERMISSION_REQUIRED,
+          schema=PostUserPararameter)
+    def collection_post(self):
+        """Create a new user."""
+        param = NewUser({'name': self.request.validated['username'],
+                         'password': self.request.validated['password']})
+        user = User.create(param)
+        log.info('Created user {} with name {}'.format(user.user_id, user.name))
+        user_url = self.request.route_path('User', user_id=user.user_id)
+        return {'location': user_url}
