abded84cf516247f7ae93d37e7e8c536aebea59b
Aymeric Barantal <mric@gandi.net>
2016-03-19 12:48:15 +0100

    Implement delete api for contact sub object

--- caliopen/api/user/contact.py
+++ caliopen/api/user/contact.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Caliopen Contact REST API."""
+from __future__ import absolute_import, print_function, unicode_literals
 
+import logging
 import json
 import colander
 from cornice.resource import resource, view
 from pyramid.response import Response
-import logging
+from pyramid.httpexceptions import HTTPBadRequest
+
 
 from caliopen.base.user.core import (Contact as CoreContact,
                                      Email as CoreEmail,
@@ -92,9 +97,7 @@ class BaseSubContactApi(Api):
     def collection_get(self):
         # XXX define filters from request
         filters = {}
-        objs = self.core_class.find_index(self.user, self.contact, filters,
-                                          limit=self.get_limit(),
-                                          offset=self.get_offset())
+        objs = self.core_class.find(self.user, self.contact)
         rets = [self.return_class.build(x).serialize() for x in objs['data']]
         return {self.namespace: rets, 'total': objs['total']}
 
@@ -106,10 +109,17 @@ class BaseSubContactApi(Api):
                   format(created.address_id, contact.contact_id))
         return return_obj.build(created).serialize()
 
+    def _delete(self, relation_id, delete_func):
+        """Delete sub object relation_id using delete_fund."""
+        contact_id = self.request.validated['contact_id']
+        contact = CoreContact.get(self.user, contact_id)
+        return getattr(contact, delete_func)(relation_id)
+
 
 class NewAddressParam(colander.MappingSchema):
 
     """Parameter to create a new postal address."""
+
     contact_id = colander.SchemaNode(colander.String(), location='path')
     label = colander.SchemaNode(colander.String(), location='body',
                                 missing=colander.drop)
@@ -120,6 +130,14 @@ class NewAddressParam(colander.MappingSchema):
     country = colander.SchemaNode(colander.String(), location='body')
     region = colander.SchemaNode(colander.String(), location='body',
                                  missing=colander.drop)
+
+
+class DeleteAddressParam(colander.MappingSchema):
+
+    """Parameter to delete an existing postal address."""
+
+    contact_id = colander.SchemaNode(colander.String(), location='path')
+    address_id = colander.SchemaNode(colander.String(), location='path')
 
 
 @resource(collection_path='/contacts/{contact_id}/addresses',
@@ -140,6 +158,18 @@ class ContactAddress(BaseSubContactApi):
                                ReturnAddress)
         return Response(status=201, body=json.dumps({'addresses': out_obj}))
 
+    @view(renderer='json', permission='authenticated',
+          schema=DeleteAddressParam)
+    def delete(self):
+        address_id = self.request.validated['address_id']
+        res = self._delete(address_id, 'delete_address')
+        if res:
+            # XXX define correct return value
+            return Response(status=200, body=json.dumps({'result': 'ok'}))
+        log.warn('Invalid return value when deleting address {}: {}'.
+                 format(address_id, res))
+        return HTTPBadRequest({'result': res, 'address_id': address_id})
+
 
 class NewEmailParam(colander.MappingSchema):
 
@@ -147,6 +177,14 @@ class NewEmailParam(colander.MappingSchema):
     contact_id = colander.SchemaNode(colander.String(), location='path')
     type = colander.SchemaNode(colander.String(), location='body')
     address = colander.SchemaNode(colander.String(), location='body')
+
+
+class DeleteEmailParam(colander.MappingSchema):
+
+    """Parameter to delete an existing email."""
+
+    contact_id = colander.SchemaNode(colander.String(), location='path')
+    address = colander.SchemaNode(colander.String(), location='path')
 
 
 @resource(collection_path='/contacts/{contact_id}/emails',
@@ -167,13 +205,33 @@ class ContactEmail(BaseSubContactApi):
                                ReturnEmail)
         return Response(status=201, body=json.dumps({'addresses': out_obj}))
 
+    @view(renderer='json', permission='authenticated',
+          schema=DeleteEmailParam)
+    def delete(self):
+        address = self.request.validated['address']
+        res = self._delete(address, 'delete_email')
+        if res:
+            # XXX define correct return value
+            return Response(status=200, body=json.dumps({'result': 'ok'}))
+        log.warn('Invalid return value when deleting email {}: {}'.
+                 format(address, res))
+        return HTTPBadRequest({'result': res, 'address_id': address})
+
 
 class NewIMParam(colander.MappingSchema):
 
-    """Parameter to create a new email."""
+    """Parameter to create a new im."""
     contact_id = colander.SchemaNode(colander.String(), location='path')
     type = colander.SchemaNode(colander.String(), location='body')
     address = colander.SchemaNode(colander.String(), location='body')
+
+
+class DeleteIMParam(colander.MappingSchema):
+
+    """Parameter to delete an existing im."""
+
+    contact_id = colander.SchemaNode(colander.String(), location='path')
+    address = colander.SchemaNode(colander.String(), location='path')
 
 
 @resource(collection_path='/contacts/{contact_id}/ims',
@@ -193,6 +251,18 @@ class ContactIM(BaseSubContactApi):
         out_obj = self._create(contact_id, im, 'add_im',
                                ReturnIM)
         return Response(status=201, body=json.dumps({'addresses': out_obj}))
+
+    @view(renderer='json', permission='authenticated',
+          schema=DeleteIMParam)
+    def delete(self):
+        address = self.request.validated['address']
+        res = self._delete(address, 'delete_im')
+        if res:
+            # XXX define correct return value
+            return Response(status=200, body=json.dumps({'result': 'ok'}))
+        log.warn('Invalid return value when deleting im {}: {}'.
+                 format(address, res))
+        return HTTPBadRequest({'result': res, 'address_id': address})
 
 
 @resource(collection_path='/contacts/{contact_id}/identities',
