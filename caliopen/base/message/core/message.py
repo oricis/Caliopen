62eebe281f3e0add1585cd83c17e89ced3f8fe63
root <root@argentina-fbx.brainstorm.fr>
2016-04-12 19:05:21 +0200

    retour a l'original.

--- caliopen/base/message/core/message.py
+++ caliopen/base/message/core/message.py
@@ -30,7 +30,6 @@ class Message(BaseUserCore, MixinCoreIndex):
     @classmethod
     def create(cls, user, message, thread, lookup):
         """Create a new message for a given user and thread."""
-        message = unicode(message, "utf-8")
         message.validate()
         parent_id = message.external_parent_id
         message_id = uuid.uuid4()
