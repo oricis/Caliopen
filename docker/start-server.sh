6edfa4a62dbddbd7ce8df2125ed2f3390370e38d
Aymeric Barantal <mric@gandi.net>
2016-05-06 09:50:37 +0200

    Set virtual_transport to 127.0.0.1 IP address

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -3,6 +3,7 @@
 [[ -z ${CALIOPEN_VIRTUAL_MAILBOX_DOMAINS} ]] && echo "unable to find CALIOPEN_VIRTUAL_MAILBOX_DOMAINS" && exit 1
 
 postconf -e virtual_mailbox_domains=${CALIOPEN_VIRTUAL_MAILBOX_DOMAINS}
+postconf -e virtual_transport=lmtp:127.0.0.1:4000
 
 caliopen/smtp/bin/lmtpd.py -f /caliopen.yaml 2>&1 >> /var/log/lmtp.log &
 service rsyslog start
