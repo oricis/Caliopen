d41272cc129c8b804d3a9b2adbdd694bbc024453
Aymeric Barantal <mric@gandi.net>
2016-05-09 11:00:17 +0200

    Make postconf command running only if env variables are set

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 [[ -z ${CALIOPEN_VIRTUAL_MAILBOX_DOMAINS} ]] && echo "unable to find CALIOPEN_VIRTUAL_MAILBOX_DOMAINS" && exit 1
-
 postconf -e virtual_mailbox_domains=${CALIOPEN_VIRTUAL_MAILBOX_DOMAINS}
-postconf -e virtual_transport=lmtp:127.0.0.1:4000
+
+[[ ! -z ${CALIOPEN_VIRTUAL_TRANSPORT} ]] && postconf -e virtual_transport=${CALIOPEN_VIRTUAL_TRANSPORT}
 
 caliopen/smtp/bin/lmtpd.py -f /caliopen.yaml 2>&1 >> /var/log/lmtp.log &
 service rsyslog start
