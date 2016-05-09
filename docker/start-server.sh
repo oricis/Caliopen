62ca151f356d3aaa21569445fc419f91cc1f345c
Aymeric Barantal <mric@gandi.net>
2016-05-09 14:43:09 +0200

    Be consistent test also for CALIOPEN_VIRTUAL_TRANSPORT variable

--- docker/start-server.sh
+++ docker/start-server.sh
@@ -3,7 +3,8 @@
 [[ -z ${CALIOPEN_VIRTUAL_MAILBOX_DOMAINS} ]] && echo "unable to find CALIOPEN_VIRTUAL_MAILBOX_DOMAINS" && exit 1
 postconf -e virtual_mailbox_domains=${CALIOPEN_VIRTUAL_MAILBOX_DOMAINS}
 
-[[ ! -z ${CALIOPEN_VIRTUAL_TRANSPORT} ]] && postconf -e virtual_transport=${CALIOPEN_VIRTUAL_TRANSPORT}
+[[ -z ${CALIOPEN_VIRTUAL_TRANSPORT} ]] && echo "unable to find CALIOPEN_VIRTUAL_TRANSPORT environment variable" && exit 1
+postconf -e virtual_transport=${CALIOPEN_VIRTUAL_TRANSPORT}
 
 caliopen/smtp/bin/lmtpd.py -f /caliopen.yaml 2>&1 >> /var/log/lmtp.log &
 service rsyslog start
