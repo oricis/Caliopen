6576f2302ca6104b9cf4745314227e516abf2663
Aymeric Barantal <mric@gandi.net>
2016-11-12 11:21:19 +0100

    Working version with started services

--- bootstrap-debian.sh
+++ bootstrap-debian.sh
@@ -2,9 +2,7 @@
 
 apt-get -y update
 apt-get -y upgrade
-apt-get install -y git libffi-dev python-pip gcc python-dev libssl-dev libev4 libev-dev
-
-apt-get install redis-server
+apt-get install -y git libffi-dev python-pip gcc python-dev libssl-dev libev4 libev-dev redis-server elasticsearch
 
 # Debian jessie setuptools is a really old version (5.1.x)
 # Install a really fresh version of setuptools
@@ -22,14 +20,10 @@ mkdir ${CALIOPEN_BASE_DIR}
 mkdir ${CALIOPEN_BASE_DIR}/ext && cd $_
 
 # Install storage engines
-CASSANDRA_VERSION="3.0.9"
-ES_VERSION="2.4.1"
+CASSANDRA_VERSION="2.2.8"
 
 wget -q http://www-eu.apache.org/dist/cassandra/${CASSANDRA_VERSION}/apache-cassandra-${CASSANDRA_VERSION}-bin.tar.gz
 tar xzf apache-cassandra-${CASSANDRA_VERSION}-bin.tar.gz
-
-wget -q https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ES_VERSION}/elasticsearch-${ES_VERSION}.tar.gz
-tar xzf elasticsearch-${ES_VERSION}.tar.gz
 
 
 # Clone repository
@@ -44,3 +38,13 @@ python setup.py develop
 cd ${CALIOPEN_BASE_DIR}/code/src/backend/interfaces/REST/py.server
 python setup.py develop
 
+# Start services
+cd ${CALIOPEN_BASE_DIR}/ext/apache-cassandra-${CASSANDRA_VERSION}
+sed -i -e '/#MAX_HEAP_SIZE=/ s/.*/MAX_HEAP_SIZE="1G"/' conf/cassandra-env.sh
+./bin/cassandra
+
+sed -i -e '/#START_DAEMON=true/ s/.*/START_DAEMON=true/' /etc/default/elasticsearch
+/etc/init.d/elasticsearch start
+
+cd ${CALIOPEN_BASE_DIR}/code/src/backend/interfaces/REST/py.server
+pserve --daemon development.ini
