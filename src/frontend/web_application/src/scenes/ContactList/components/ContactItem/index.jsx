a7b097dea7027015b49198138137824fd84c8349
Mylene <mylene.bressan@gandi.net>
2016-12-08 16:15:52 +0100

    Frontend  ContactList component migration

--- src/frontend/web_application/src/scenes/ContactList/components/ContactItem/index.jsx
+++ src/frontend/web_application/src/scenes/ContactList/components/ContactItem/index.jsx
@@ -1,3 +1,26 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import ContactAvatarLetter from '../../../../components/ContactAvatarLetter';
+
+const ContactItem = ({ contact }) => (
+  <Link
+    to={`/contacts/${contact.contact_id}`}
+    className="s-contact-list__contact m-block-list__item-content m-block-list__item-content--link"
+  >
+    <ContactAvatarLetter contact={contact} />
+    <div className="s-contact-list__col-name m-text-line">
+      {contact.title}
+    </div>
+  </Link>
+  );
+
+
+ContactItem.propTypes = {
+  contact: PropTypes.shape({}),
+};
+
+export default ContactItem;
+
 // const ContactItem = `
 //   <Link to={`/contacts/${contact.contact_id}`}
 //     class="s-contact-list__contact m-block-list__item-content m-block-list__item-content--link"
