c0594d6d541ea15e07cd378aeb0169476f991e00
David Epely <esion99@gmail.com>
2017-01-05 14:36:42 +0100

    refactor frontend express app in composable parts

--- src/frontend/web_application/server/app.js
+++ src/frontend/web_application/server/app.js
@@ -1,21 +1,13 @@
-const path = require('path');
 const express = require('express');
-const locale = require('locale');
-const cookieParser = require('cookie-parser');
-
-const supportedLocales = ['en', 'fr'];
+const applySSR = require('./ssr');
+const applyAssets = require('./assets');
 
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
 //-------
-// assets & eventual bundle.js
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
-//-------
-app.use(locale(supportedLocales));
-app.use(cookieParser());
-app.get('*', require('./middleware/ssr.js'));
+applySSR(app);
+applyAssets(app);
 
 module.exports = app;
