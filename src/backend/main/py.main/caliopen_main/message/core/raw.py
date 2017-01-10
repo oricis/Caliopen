94686158068bcfb9af06c4f3cef0be4bb3f87d56
Stan <stan@sapienssapide.fr>
2017-01-10 12:01:46 +0100

    create a RawInboundMessage model that do not belong to an user. Delivery process now rely on that.

--- src/backend/main/py.main/caliopen_main/message/core/raw.py
+++ src/backend/main/py.main/caliopen_main/message/core/raw.py
@@ -5,10 +5,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 import uuid
 import logging
 
-from caliopen_storage.core import BaseUserCore
+from caliopen_storage.core import BaseUserCore, BaseCore
 from caliopen_storage.exception import NotFound
 
-from ..store import RawMessage as ModelRaw
+from ..store import (RawMessage as ModelRaw,
+                     RawInboundMessage as ModelRawInbound)
 from ..format import MailMessage
 
 log = logging.getLogger(__name__)
@@ -39,6 +40,35 @@ class RawMessage(BaseUserCore):
         except NotFound:
             return None
 
+    def parse(self):
+        """Parse raw message to get a formatted object."""
+        return MailMessage(self.data)
+
+
+class RawInboundMessage(BaseCore):
+
+    """
+    Raw Inbound message core.
+
+    Store in binary form any message before processing
+    """
+
+    _model_class = ModelRawInbound
+    _pkey_name = 'raw_msg_id'
+
+    @classmethod
+    def create(cls, raw):
+        """Create raw message."""
+        key = uuid.uuid4()
+        return super(RawInboundMessage, cls).create(raw_msg_id=key, data=raw)
+
+    @classmethod
+    def get(cls, raw_msg_id):
+        """Get raw message by raw_msg_id."""
+        try:
+            return super(RawInboundMessage, cls).get(raw_msg_id)
+        except NotFound:
+            return None
 
     def parse(self):
         """Parse raw message to get a formatted object."""
--- src/backend/main/py.main/caliopen_main/message/store/__init__.py
+++ src/backend/main/py.main/caliopen_main/message/store/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-from .message import RawMessage, Message, MessageRecipient
+from .message import RawMessage, RawInboundMessage, Message, MessageRecipient
 from .message_index import IndexedMessage
 from .thread import (Thread, ThreadCounter, ThreadRecipientLookup,
                      ThreadExternalLookup, ThreadMessageLookup)
