8bd29117fa3512060665b4ff34c1b88b20e9e490
David Epely <esion99@gmail.com>
2017-01-11 09:28:20 +0100

    production optimization for webpack/react

--- src/frontend/web_application/webpack/config.js
+++ src/frontend/web_application/webpack/config.js
@@ -50,6 +50,9 @@ module.exports = {
       new webpack.DefinePlugin({
         BUILD_TARGET: JSON.stringify(buildTarget),
         CALIOPEN_ENV: JSON.stringify(process.env.NODE_ENV),
+        'process.env': {
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        },
       }),
     ];
     if (DASHBOARD) {
