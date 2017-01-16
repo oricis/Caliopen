7c7fce70c1726c608f9e49c2b4e8faff0b8d0577
Mylene <mylene@mylene.home>
2017-01-16 12:07:24 +0100

    Signup Form css

--- src/frontend/web_application/src/components/SignupForm/index.jsx
+++ src/frontend/web_application/src/components/SignupForm/index.jsx
@@ -19,62 +19,55 @@ class SignupForm extends Component {
 
   render() {
     return (
-      <FormGrid className="m-account-creation-form" name="ac_form">
-        <Fieldset>
-          <FormRow>
-            <FormColumn className="m-account-creation-form__brand">
-              <Brand />
-            </FormColumn>
-          </FormRow>
-          <FormRow>
-            <FormColumn size="expand">
-              <Title>Create your account</Title>
-            </FormColumn>
-          </FormRow>
-          {this.state.errors.length !== 0 && (
+      <div className="s-signup">
+        <Brand />
+        <FormGrid className="m-signup-form" name="ac_form">
+          <Fieldset>
             <FormRow>
               <FormColumn size="expand">
-                <FieldErrors className="m-account-creation-form__errors" errors={this.state.errors} />
+                <Title>Create your account</Title>
               </FormColumn>
             </FormRow>
-          )}
-          <FormRow>
-            <FormColumn size="expand">
-              <TextFieldGroup
-                name="username"
-                placeholder="username"
-              />
-            </FormColumn>
-          </FormRow>
-          <FormRow>
-            <FormColumn size="expand">
-              <TextFieldGroup
-                name="password"
-                placeholder="password"
-                type="password"
-              />
-              {this.state.props.passwordStrenght.length !== 0 && (
-                <PasswordStrenght strenght={this.state.props.passwordStrenght} />
-              )}
-            </FormColumn>
-          </FormRow>
-          <FormRow>
-            <FormColumn size="expand" className="m-account-creation-form__terms">
-              <Switch label="I agree Terms and conditions" duplicateLabel />
-            </FormColumn>
-          </FormRow>
-          <FormRow className="m-account-creation-form__submit">
-            <FormColumn size="expand" className="m-im-form__action">
-              <Button type="submit" expanded plain> Submit</Button>
-            </FormColumn>
-          </FormRow>
-          <FormRow className="m-account-creation-form__submit">
-            <FormColumn size="expand" className="m-im-form__action">
-              <Link to="#">Cancel</Link>
-            </FormColumn>
-          </FormRow>
-        </Fieldset>
-      </FormGrid>
+            {this.state.errors.length !== 0 && (
+              <FormRow>
+                <FormColumn size="expand">
+                  <FieldErrors className="m-signup-form__errors" errors={this.state.errors} />
+                </FormColumn>
+              </FormRow>
+            )}
+            <FormRow>
+              <FormColumn size="expand">
+                <TextFieldGroup
+                  name="username"
+                  placeholder="username"
+                />
+              </FormColumn>
+            </FormRow>
+            <FormRow>
+              <FormColumn size="expand">
+                <TextFieldGroup
+                  name="password"
+                  placeholder="password"
+                  type="password"
+                />
+                {this.state.props.passwordStrenght.length !== 0 && (
+                  <PasswordStrenght strenght={this.state.props.passwordStrenght} />
+                )}
+              </FormColumn>
+            </FormRow>
+            <FormRow>
+              <FormColumn size="expand" className="m-signup-form__terms">
+                <Switch label="I agree Terms and conditions" duplicateLabel />
+              </FormColumn>
+            </FormRow>
+            <FormRow className="m-signup-form__submit">
+              <FormColumn size="expand" className="m-im-form__action">
+                <Button type="submit" expanded plain> Submit</Button>
+              </FormColumn>
+            </FormRow>
+          </Fieldset>
+        </FormGrid>
+      </div>
     );
   }
 }
