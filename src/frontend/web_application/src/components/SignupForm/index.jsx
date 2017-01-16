a0abfeaea22975688e2570cd7463f962fe0979b1
Mylene <mylene.bressan@gandi.net>
2017-01-16 16:42:38 +0100

    Fixes for SignupForm

--- src/frontend/web_application/src/components/SignupForm/index.jsx
+++ src/frontend/web_application/src/components/SignupForm/index.jsx
@@ -10,7 +10,7 @@ class SignupForm extends Component {
     super(props);
     this.state = {
       props: {
-        passwordStrenght: 'moderate',
+        passwordStrenght: '',
       },
       errors: [],
     };
