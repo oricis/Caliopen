ec84059f3e7526315a6d5f25b4de6ce5dbb2ad83
Mylene <mylene.bressan@gandi.net>
2017-01-23 11:24:42 +0100

    dispays <DevicesManagment />

--- src/frontend/web_application/src/scenes/Settings/components/DevicesManagment/index.jsx
+++ src/frontend/web_application/src/scenes/Settings/components/DevicesManagment/index.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Icon from '../../../../components/Icon';
+import Subtitle from '../../../../components/Subtitle';
+import { FormGrid, FormRow, FormColumn, SelectFieldGroup } from '../../../../components/form';
 
 import './style.scss';
 
@@ -12,7 +15,21 @@ class DevicesManagment extends Component {
 
   render() {
     return (
-      <div>Devices managment</div>
+      <FormGrid>
+        <FormRow>
+          <FormColumn size="shrink">
+            <label htmlFor="select-device">Select a device to edit:</label>
+          </FormColumn>
+          <FormColumn size="shrink">
+            <SelectFieldGroup
+              label="Select a device to edit"
+              name="select-device"
+              showLabelforSr
+              options={[{ value: 'PC maison', label: 'PC maison' }, { value: 'Android', label: 'Android' }, { value: 'Laptop', label: 'Laptop' }]}
+            />
+          </FormColumn>
+        </FormRow>
+      </FormGrid>
     );
   }
 }
--- src/frontend/web_application/src/scenes/Settings/index.jsx
+++ src/frontend/web_application/src/scenes/Settings/index.jsx
@@ -1,31 +1,45 @@
 import React, { Component } from 'react';
 import DevicesManagment from './components/DevicesManagment';
-import BlockList, { ItemContent } from '../../components/BlockList';
-
+import Button from '../../components/Button';
+import Icon from '../../components/Icon';
+import { FormGrid, FormRow, FormColumn } from '../../components/form';
 
 import './style.scss';
+
+const navigationLinks = [
+  /* eslint-disable */
+  {'link': {'title': 'User Interface', 'href': '#', icon: 'window-maximize', active: false}},
+  {'link': {'title': 'View', 'href': '#', icon: 'th-large', active: false}},
+  {'link': {'title': 'Contacts', 'href': '#', icon: 'users', active: false}},
+  {'link': {'title': 'Calendar', 'href': '#', icon: 'calendar', active: false}},
+  {'link': {'title': 'Server', 'href': '#', icon: 'server', active: false}},
+  {'link': {'title': 'Devices', 'href': '#', icon: 'laptop', active: true}},
+];
+  /* eslint-enable */
 
 class Settings extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
     };
   }
 
   render() {
     return (
       <div className="s-settings">
-        <BlockList className="s-settings__nav">
-          <ItemContent>User Interface</ItemContent>
-          <ItemContent>View</ItemContent>
-          <ItemContent>Contacts</ItemContent>
-          <ItemContent>Calendar</ItemContent>
-          <ItemContent>Server</ItemContent>
-          <ItemContent>Accounts</ItemContent>
-          <ItemContent>Devices</ItemContent>
-        </BlockList>
-        <div className="s-settings__pannel"><DevicesManagment /></div>
+        <FormGrid className="s-settings__nav">
+          <FormRow>
+            {navigationLinks.map(nav =>
+              <FormColumn size="shrink">
+                <Button active={nav.link.active} className="s-settings__nav-item"><Icon type={nav.link.icon} spaced /> {nav.link.title}</Button>
+              </FormColumn>
+            )
+            }
+          </FormRow>
+        </FormGrid>
+        <div className="s-settings__pannel">
+          <DevicesManagment />
+        </div>
       </div>
     );
   }
