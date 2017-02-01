92a84f2ad86b8826d36158744e5fae1fe6aafa2a
Aymeric Barantal <mric@gandi.net>
2017-02-01 20:58:25 +0100

    Declare Device storage part when running cli setup command

--- src/backend/main/py.main/caliopen_main/user/core/__init__.py
+++ src/backend/main/py.main/caliopen_main/user/core/__init__.py
@@ -3,9 +3,11 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from .user import User, Counter, Tag, FilterRule
 from .contact import Contact, ContactLookup, PublicKey
+from .device import Device, DevicePublicKey, DeviceLocation
 
 
 __all__ = [
     'User', 'Counter', 'Tag', 'FilterRule',
     'Contact', 'ContactLookup', 'PublicKey',
+    'Device', 'DevicePublicKey', 'DeviceLocation',
 ]
