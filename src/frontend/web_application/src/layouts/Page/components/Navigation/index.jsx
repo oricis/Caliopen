b2c6458788cc6aa51e09028af480d6633df1a05a
David Epely <esion99@gmail.com>
2017-02-08 18:09:54 +0100

    Migrate StickyNavBar component

--- src/frontend/web_application/src/layouts/Page/components/Navigation/index.jsx
+++ src/frontend/web_application/src/layouts/Page/components/Navigation/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ApplicationSwitcher from './components/ApplicationSwitcher';
 import Navbar, { Tab } from './components/Navbar';
+import StickyNavbar from './components/StickyNavBar';
 import './style.scss';
 
 const Navigation = () => (
   <Navbar className="l-navigation hide-for-small-only">
-    <div className="l-navigation__wrapper" sticky-navbar-class="l-navigation__wrapper--sticky">
+    <StickyNavbar className="l-navigation__wrapper" stickyClassName="l-navigation__wrapper--sticky">
       <Tab className="l-navigation__application-switcher" active>
         <ApplicationSwitcher />
       </Tab>
@@ -15,7 +16,7 @@ const Navigation = () => (
       <div className="l-navigation__sliders-toggle">
         <Tab last><sliders-container /></Tab>
       </div>
-    </div>
+    </StickyNavbar>
   </Navbar>
 );
 
