d33020d8ac0e9f9734ff3ab3d4cb5a93220ad93e
Aymeric Barantal <mric@gandi.net>
2017-02-10 11:59:10 +0100

    A PATCH /devices api

--- src/backend/interfaces/REST/py.server/caliopen_api/user/device.py
+++ src/backend/interfaces/REST/py.server/caliopen_api/user/device.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+"""Caliopen ReST API for user device management."""
 from __future__ import absolute_import, print_function, unicode_literals
 
 import logging
 
-
+from pyramid.response import Response
 from caliopen_main.objects.device import Device as ObjectDevice
 from caliopen_main.user.core import Device as CoreDevice
 
@@ -12,6 +13,7 @@ from cornice.resource import resource, view
 from ..base import Api
 from ..base.context import DefaultContext
 
+from ..base.exception import MergePatchError
 
 log = logging.getLogger(__name__)
 
@@ -22,10 +24,10 @@ log = logging.getLogger(__name__)
           factory=DefaultContext
           )
 class DeviceAPI(Api):
-
     """Device management API."""
 
     def __init__(self, request, context):
+        """Create an instance of Device API."""
         self.request = request
         self.user = request.authenticated_userid
 
@@ -52,9 +54,33 @@ class DeviceAPI(Api):
 
     @view(renderer='json', permission='authenticated')
     def get(self):
+        """Get a complete device information."""
         device_id = self.request.swagger_data["device_id"]
         device = ObjectDevice(self.user.user_id, device_id=device_id)
         device.get_db()
         device.unmarshall_db()
         log.info('Found device {}'.format(device))
         return device.marshall_json_dict()
+
+    @view(renderer='json', permission='authenticated')
+    def patch(self):
+        """Update a device with payload.
+
+        method follows the rfc5789 PATCH and rfc7396 Merge patch
+        specifications, + 'current_state' caliopen's specs.
+        stored messages are modified according to the fields within the
+        payload, ie payload fields squash existing db fields, no other
+        modification done. If message doesn't existing, response is 404.
+        If payload fields are not conform to the message db schema, response is
+        422 (Unprocessable Entity).
+        Successful response is 204, without a body.
+        """
+        device_id = self.request.swagger_data["device_id"]
+        patch = self.request.json
+
+        device = ObjectDevice(self.user.user_id, device_id=device_id)
+        error = device.apply_patch(patch, db=True)
+        if error is not None:
+            raise MergePatchError(error)
+
+        return Response(None, 204)
