7b099a25cf48b6dc5e93ed1aa8e3a8a46d831636
Mylene <mylene.bressan@gandi.net>
2017-02-14 11:56:51 +0100

    Improve TagSearch

--- src/frontend/web_application/src/components/TagsForm/components/TagSearch/index.jsx
+++ src/frontend/web_application/src/components/TagsForm/components/TagSearch/index.jsx
@@ -1,50 +1,30 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import Button from '../../../Button';
 import Icon from '../../../Icon';
 import { TextFieldGroup } from '../../../form';
 
 import './style.scss';
 
-class TagSearch extends Component {
-  static propTypes = {
-    __: PropTypes.func,
-  };
+const TagSearch = ({ __ }) => {
+  const noop = str => str;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      edit: false,
-    };
+  return (
+    <div className="m-tags-search">
+      <TextFieldGroup
+        id="tags-search"
+        name="tags-search"
+        className="m-tags-search__input"
+        label={__('tags.form.search.label')}
+        placeholder={__('tags.form.search.placeholder')}
+        showLabelforSr
+      />
+      <Button inline onClick={noop}><Icon type="search" /></Button>
+    </div>
+  );
+};
 
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-
-  handleClick() {
-    this.setState(prevState => ({
-      edit: !prevState.edit,
-    }));
-  }
-
-
-  render() {
-    const { __ } = this.props;
-
-    return (
-      <div className="m-tags-search">
-        <TextFieldGroup
-          id="tags-search"
-          name="tags-search"
-          className="m-tags-search__input"
-          label={__('tags.form.search.label')}
-          placeholder={__('tags.form.search.placeholder')}
-          onChange={this.handleInputChange}
-          showLabelforSr
-        />
-        <Button inline onClick={this.handleSubmit}><Icon type="search" /></Button>
-      </div>
-    );
-  }
-}
+TagSearch.propTypes = {
+  __: PropTypes.func,
+};
 
 export default TagSearch;
