7010babf9486038388a6b7b36af724cd4676fa0d
David Epely <esion99@gmail.com>
2017-03-14 15:18:44 +0100

    WIP: Add prism mock launcher

---
+++ devtools/prism.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PRISM_VERSION=v0.6.15
+PROJECT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+API_PORT=31415
+
+set -e
+
+if [ ! -f $PROJECT_DIRECTORY/prism_linux_amd64 ]; then
+  wget -O  $PROJECT_DIRECTORY/prism_linux_amd64 https://github.com/stoplightio/prism/releases/download/$PRISM_VERSION/prism_linux_amd64
+  chmod +x $PROJECT_DIRECTORY/prism_linux_amd64
+fi
+
+$PROJECT_DIRECTORY/prism_linux_amd64 run --mock --port $API_PORT --spec $PROJECT_DIRECTORY/doc/api/swagger.json
+
+set +e
