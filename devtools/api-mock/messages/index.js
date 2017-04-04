00ce54e79029d53a48547ddac685a6c151ee601e
David Epely <esion99@gmail.com>
2017-04-04 17:30:39 +0200

    update api-mock following new specifications

--- devtools/api-mock/messages/index.js
+++ devtools/api-mock/messages/index.js
@@ -4,18 +4,28 @@ import createCollectionMiddleware from '../collection-middleware';
 
 const actions = {
   get: createAction('Get messages'),
+  post: createAction('Post message'),
 };
 
 const selectors = {
   all: () => state => state.messages,
+  last: () => state => [...state.messages].pop(),
   byDiscussionId: ({ discussion_id }) => createSelector(
     selectors.all(),
     messages => messages.filter(message => message.discussion_id === discussion_id)
   ),
+  lastLocation: () => createSelector(
+    selectors.last(),
+    message => ({ location: `/api/v1/messages/${message.message_id}` })
+  )
 };
 
 const reducer = {
   [actions.get]: state => state,
+  [actions.post]: (state, params) => ([
+    ...state,
+    { ...params.body, message_id: uuidv1(), is_draft: true, is_unread: false, type: 'email' },
+  ]),
 };
 
 const routes = {
@@ -24,6 +34,11 @@ const routes = {
     selector: selectors.byDiscussionId,
     status: 200,
     middlewares: [createCollectionMiddleware('messages')],
+  },
+  'POST /': {
+    action: actions.post,
+    selector: selectors.lastLocation,
+    status: 200,
   },
 };
 
