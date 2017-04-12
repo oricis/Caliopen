67d1bf6bd806301b89960289c7dcd4fd19ffef72
Stan <git-ssa@sapienssapide.com>
2017-04-12 19:45:19 +0200

    Refactor the way nats message string are build in caliopen_nats
    Add a more 'pythonic' way of building the json strings for nats messages.

--- src/backend/interfaces/NATS/py.client/caliopen_nats/subscribers.py
+++ src/backend/interfaces/NATS/py.client/caliopen_nats/subscribers.py
@@ -5,13 +5,17 @@ from caliopen_main.message.delivery import UserMessageDelivery
 
 log = logging.getLogger(__name__)
 
-class InboundEmail(object):
 
+class InboundEmail(object):
     def __init__(self, natsConn):
         self.deliver = UserMessageDelivery()
         self.natsConn = natsConn
 
     def handler(self, msg):
+        error_str = 'inbound email message process failed'
+        success_str = 'OK : inbound email message proceeded'
+        nats_error = '{{"error":{error},"message":"{error_str}"}}'
+        nats_success = '{{"message":"{success_str}"}}'
         payload = json.loads(msg.data)
         if payload[u'order'] == "process_message":
             try:
@@ -19,22 +23,23 @@ class InboundEmail(object):
                                              payload[u'message_id'])
             except Exception as exc:
                 print("deliver process failed : {}".format(str(exc)))
-                self.natsConn.publish(msg.reply, '{"error":' +
-                                      json.dumps(str(exc.message)) + ',"message":'
-                                      '"inbound email message process failed"}')
+                self.natsConn.publish(msg.reply,
+                                      nats_error.format(error=json.dumps(
+                                          str(exc.message)),
+                                          error_str=error_str))
                 return exc
-            self.natsConn.publish(msg.reply, '{"message":' +
-                                             '"OK : inbound email message proceeded"}')
+            self.natsConn.publish(msg.reply,
+                                  nats_success.format(success_str=success_str))
         if payload[u'order'] == "process_email_raw":
             try:
                 self.deliver.process_email_raw(payload[u'user_id'],
                                                payload[u'raw_msg_id'])
             except Exception as exc:
                 print("deliver process failed : {}".format(str(exc)))
-                self.natsConn.publish(msg.reply, '{"error":' +
-                                      json.dumps(
-                                          str(exc.message)) + ',"message":'
-                                                              '"inbound email process failed"}')
+                self.natsConn.publish(msg.reply,
+                                      nats_error.format(error=json.dumps(
+                                          str(exc.message)),
+                                          error_str=error_str))
                 return exc
-            self.natsConn.publish(msg.reply, '{"message":' +
-                                  '"OK : inbound email proceeded"}')
+            self.natsConn.publish(msg.reply,
+                                  nats_success.format(success_str=success_str))
