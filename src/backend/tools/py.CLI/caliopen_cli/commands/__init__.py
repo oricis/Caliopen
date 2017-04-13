1dce21dec47ef85cbf3492081d289f7504dfcd90
lisonferez <lisonfer@orange.fr>
2017-04-13 14:41:00 +0200

    Feature/backend/import vcard (#236)
    * Add vobject library as dependency
    
    * Add import_vcard command to caliopen_cli
    
    * Modify after review
    
    * Modify after review 2

--- src/backend/tools/py.CLI/caliopen_cli/commands/__init__.py
+++ src/backend/tools/py.CLI/caliopen_cli/commands/__init__.py
@@ -2,4 +2,5 @@ from .shell import shell
 from .setup_storage import setup_storage
 from .create_user import create_user
 from .import_email import import_email
+from .import_vcard import import_vcard
 from .dump_model import dump_model
