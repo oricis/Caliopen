78f9dca4ea36d7426320cdfaa295ef0698512ef3
David Epely <esion99@gmail.com>
2017-05-02 16:38:10 +0200

    Implement subrequest headers for SSR
    + remove prefetch on SSR

---
+++ src/frontend/web_application/server/api/lib/sub-request-manager.js
@@ -0,0 +1,6 @@
+const CALIOPEN_SUBREQUEST = 'X-Caliopen-SSR';
+const CALIOPEN_SUBREQUEST_VALUE = 'SSR';
+
+export const getSubRequestHeaders = () => ({ [CALIOPEN_SUBREQUEST]: CALIOPEN_SUBREQUEST_VALUE });
+
+export const isSubRequest = req => req.headers[CALIOPEN_SUBREQUEST] === CALIOPEN_SUBREQUEST_VALUE;
--- src/frontend/web_application/server/auth/middlewares/catch-login-errors.js
+++ src/frontend/web_application/server/auth/middlewares/catch-login-errors.js
@@ -1,5 +1,7 @@
+const { isSubRequest } = require('../../api/lib/sub-request-manager');
+
 const catchLoginErrorsMiddleware = (err, req, res, next) => {
-  if (err.status === 401 && !req.xhr && req.ip.indexOf('127.0.0.1') === -1) {
+  if (err.status === 401 && !req.xhr && !isSubRequest(req)) {
     const redirectTo = `/auth/signin?redirect=${req.originalUrl.split('?')[0]}`;
     res.redirect(redirectTo);
 
