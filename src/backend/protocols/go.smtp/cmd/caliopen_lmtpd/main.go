1c3ff9483c3b214be61d362b941ff80908fc2ca5
Stan <git-ssa@sapienssapide.com>
2017-05-10 11:03:46 +0200

    Rename lmtpd/cli-cmds to lmtpd/cli_cmds

--- src/backend/protocols/go.smtp/cmd/caliopen_lmtpd/main.go
+++ src/backend/protocols/go.smtp/cmd/caliopen_lmtpd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/CaliOpen/Caliopen/src/backend/protocols/go.smtp/cmd/caliopen_lmtpd/cli-cmds"
+	"github.com/CaliOpen/Caliopen/src/backend/protocols/go.smtp/cmd/caliopen_lmtpd/cli_cmds"
 	"os"
 )
 
