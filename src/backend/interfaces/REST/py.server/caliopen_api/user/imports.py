78eae817217f1c53b4e3e75b753ab3b45b9e6c4f
lisonferez <lisonfer@orange.fr>
2017-06-27 09:49:18 +0200

    Fix/backend/345 (#351)
    * Resolve conflicts
    
    * Fix valid number phone and mail address

--- src/backend/interfaces/REST/py.server/caliopen_api/user/imports.py
+++ src/backend/interfaces/REST/py.server/caliopen_api/user/imports.py
@@ -9,9 +9,6 @@ from pyramid.response import Response
 from caliopen_main.parsers.vcard import parse_vcards
 from caliopen_main.parsers.vcard import read_file
 
-import logging
-log = logging.getLogger(__name__)
-
 from ..base.exception import (ValidationError,
                               Unprocessable)
 
