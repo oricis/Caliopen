dd98f3c288b3fe95fc301ca00d457ea97f315e78
David Epely <esion99@gmail.com>
2017-07-07 17:40:47 +0200

    functionnal tests & mock fixtures

--- src/frontend/web_application/test/functional/features/20-message-list-spec.js
+++ src/frontend/web_application/test/functional/features/20-message-list-spec.js
@@ -17,4 +17,50 @@ describe('Message list', () => {
       .then(() => browser.wait(EC.presenceOf($('.m-message-list')), 5 * 1000))
       .then(() => expect(element.all(by.css('.m-message')).count()).toEqual(2));
   });
+
+  it('removes a message', () => {
+    browser.get('/')
+      .then(() => browser.wait(EC.presenceOf($('.s-discussion-list__thread')), 5 * 1000))
+      .then(() => element(by.cssContainingText(
+        '.s-discussion-list__thread',
+        'removeme@caliopen.local, john@caliopen.local'
+      )).click())
+      .then(() => browser.wait(EC.presenceOf($('.m-message-list')), 5 * 1000))
+      .then(() => expect(element.all(by.css('.m-message')).count()).toEqual(2))
+      .then(() => {
+        const topBar = element(by.cssContainingText('.m-message__top-bar', 'removeme@caliopen.local'));
+
+        return topBar.element(by.css('.m-message__actions-switcher')).click()
+          .then(() =>
+            topBar.element(by.cssContainingText('.m-message-actions-container__action', 'Supprimer')
+              .click())
+          );
+      })
+      .then(() => expect(element.all(by.css('.m-message')).count()).toEqual(1))
+      .then(() => {
+        const topBar = element(by.cssContainingText('.m-message__top-bar', 'john@caliopen.local'));
+
+        return topBar.element(by.css('.m-message__actions-switcher')).click()
+          .then(() =>
+            topBar.element(by.cssContainingText('.m-message-actions-container__action', 'Supprimer')
+              .click())
+          );
+      })
+      .then(() => browser.wait(EC.presenceOf($('.s-discussion-list__thread')), 5 * 1000))
+    ;
+  });
+
+  it('removes all messages', () => {
+    browser.get('/')
+      .then(() => browser.wait(EC.presenceOf($('.s-discussion-list__thread')), 5 * 1000))
+      .then(() => element(by.cssContainingText(
+        '.s-discussion-list__thread',
+        'removeall@caliopen.local, john@caliopen.local'
+      )).click())
+      .then(() => browser.wait(EC.presenceOf($('.m-message-list')), 5 * 1000))
+      .then(() => expect(element.all(by.css('.m-message')).count()).toEqual(1))
+      .then(() => element(by.cssContainingText('.m-message-list__action', 'Supprimer')).click())
+      .then(() => browser.wait(EC.presenceOf($('.s-discussion-list__thread')), 5 * 1000))
+    ;
+  });
 });
