3ceb32f948048755d914d6c4b5f1b699ada60836
David Epely <esion99@gmail.com>
2017-08-10 11:15:57 +0200

    reactor signup form to import axync zxcvbn
    + refactor SignupForm with arrow functions for handlers

--- src/frontend/web_application/src/scenes/Auth/index.jsx
+++ src/frontend/web_application/src/scenes/Auth/index.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Loadable from 'react-loadable';
 import AuthPage from '../../layouts/AuthPage';
 import Signin from '../Signin';
-import BaseSpinner from '../../components/Spinner';
-
-const Spinner = () => (<BaseSpinner isLoading />);
-
-const AsyncSignup = Loadable({
-  loader: () => import('../Signup'),
-  loading: Spinner,
-});
+import Signup from '../Signup';
 
 const Auth = () => (
   <AuthPage>
     <Switch>
       <Route exact path="/auth/"><Redirect to="signin" /></Route>
       <Route path="/auth/signin" component={Signin} />
-      <Route path="/auth/signup" component={AsyncSignup} />
+      <Route path="/auth/signup" component={Signup} />
       <Route path="/auth/signout"><Redirect to="signin" /></Route>
     </Switch>
   </AuthPage>
