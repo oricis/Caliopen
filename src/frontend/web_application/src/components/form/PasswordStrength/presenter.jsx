c1130f0aff837e6065c8bb481ff450e0a41dea96
Mylene <mylene.bressan@gandi.net>
2017-09-06 08:26:52 +0200

    Fix PasswordStrength locales and styles

--- src/frontend/web_application/src/components/form/PasswordStrength/presenter.jsx
+++ src/frontend/web_application/src/components/form/PasswordStrength/presenter.jsx
@@ -1,58 +1,66 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Icon from '../../../components/Icon';
 import './style.scss';
 
-const PasswordStrength = ({ strength, className, __ }) => {
-  const feedbacks = {
-    weak: __('password_strength.feedback.weak'),
-    moderate: __('password_strength.feedback.moderate'),
-    strong: __('password_strength.feedback.strong'),
+class PasswordStrength extends PureComponent {
+  static propTypes = {
+    strength: PropTypes.number.isRequired,
+    className: PropTypes.string,
+    __: PropTypes.func.isRequired,
   };
 
-  const classNameModifiers = {
-    weak: 'm-password-strength--weak',
-    moderate: 'm-password-strength--moderate',
-    strong: 'm-password-strength--strong',
+  static defaultProps = {
+    className: null,
   };
 
-  let key;
-  switch (true) {
-    default:
-    case strength <= 1:
-      key = 'weak';
-      break;
-    case strength >= 2 && strength <= 3:
-      key = 'moderate';
-      break;
-    case strength >= 4:
-      key = 'strong';
-      break;
-  }
-  const passwordStrengthClassName = classnames(
-    'm-password-strength',
-    classNameModifiers[key],
-    className
-  );
+  render() {
+    const { strength, className, __ } = this.props;
 
-  return (
-    <div className={passwordStrengthClassName}>
-      <div className="m-password-strength__graph">
-        <Icon type="lock" className="m-password-strength__icon" />
-        <div className="m-password-strength__guide">
-          <div className="m-password-strength__bar" />
+    const feedbacks = {
+      weak: __('password_strength.feedback.weak'),
+      moderate: __('password_strength.feedback.moderate'),
+      strong: __('password_strength.feedback.strong'),
+    };
+
+    const classNameModifiers = {
+      weak: 'm-password-strength--weak',
+      moderate: 'm-password-strength--moderate',
+      strong: 'm-password-strength--strong',
+    };
+
+    let key;
+    switch (true) {
+      default:
+      case strength <= 1:
+        key = 'weak';
+        break;
+      case strength >= 2 && strength <= 3:
+        key = 'moderate';
+        break;
+      case strength >= 4:
+        key = 'strong';
+        break;
+    }
+    const passwordStrengthClassName = classnames(
+      'm-password-strength',
+      classNameModifiers[key],
+      className
+    );
+
+    return (
+      <div className={passwordStrengthClassName}>
+        <div className="m-password-strength__graph">
+          <Icon type="lock" className="m-password-strength__icon" />
+          <div className="m-password-strength__guide">
+            <div className="m-password-strength__bar" />
+          </div>
+          <span className="m-password-strength__text">{feedbacks[key]}</span>
         </div>
       </div>
-      <span className="m-password-strength__text">{feedbacks[key]}</span>
-    </div>
-  );
-};
-
-PasswordStrength.propTypes = {
-  strength: PropTypes.number.isRequired,
-  className: PropTypes.string,
-  __: PropTypes.func.isRequired,
-};
+    );
+  }
+}
 
 export default PasswordStrength;
