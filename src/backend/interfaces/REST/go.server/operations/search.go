fdb18216d6b874d7d70a240718320b04081ea5c0
Stan <git-ssa@sapienssapide.com>
2017-09-13 12:20:24 +0200

    Define HTTP interface for search operations (swagger).

---
+++ src/backend/interfaces/REST/go.server/operations/search.go
@@ -0,0 +1,14 @@
+package operations
+
+import (
+	"gopkg.in/gin-gonic/gin.v1"
+	"net/http"
+)
+
+func SimpleSearch(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
+
+func AdvancedSearch(ctx *gin.Context) {
+	ctx.AbortWithStatus(http.StatusNotImplemented)
+}
