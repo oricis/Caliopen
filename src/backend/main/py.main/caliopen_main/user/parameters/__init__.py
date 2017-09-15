489124ceac3170cd377c73d9e08d0838c4aa55c7
Aymeric Barantal <mric@gandi.net>
2017-09-15 12:05:07 +0200

    validate input settings and inject for user creation

--- src/backend/main/py.main/caliopen_main/user/parameters/__init__.py
+++ src/backend/main/py.main/caliopen_main/user/parameters/__init__.py
@@ -7,11 +7,15 @@ from caliopen_main.common.parameters.types import InternetAddressType, PhoneNumb
 
 from .tag import NewUserTag, UserTag
 from .device import NewDevice, Device
-from .identity import Identity, LocalIdentity, NewRemoteIdentity, RemoteIdentity
+from .identity import Identity, LocalIdentity
+from .identity import NewRemoteIdentity, RemoteIdentity
+from .settings import Settings
 
 __all__ = [
     'InternetAddressType', 'PhoneNumberType',
     'NewUser', 'User', 'NewRule', 'Recipient',
     'NewUserTag', 'UserTag', 'NewDevice', 'Device',
-    'Identity', 'LocalIdentity', 'NewRemoteIdentity', 'RemoteIdentity',
+    'Identity', 'LocalIdentity',
+    'NewRemoteIdentity', 'RemoteIdentity',
+    'Settings',
 ]
