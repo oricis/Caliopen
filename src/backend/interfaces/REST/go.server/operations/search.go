0006206e8b1ae5530569ddcc92d7d0722cfafb7d
Stan <git-ssa@sapienssapide.com>
2017-09-21 15:25:44 +0200

    Validate HTTP query request and return multiple errors.

--- src/backend/interfaces/REST/go.server/operations/search.go
+++ src/backend/interfaces/REST/go.server/operations/search.go
@@ -1,6 +1,7 @@
 package operations
 
 import (
+	"errors"
 	. "github.com/CaliOpen/Caliopen/src/backend/defs/go-objects"
 	"github.com/CaliOpen/Caliopen/src/backend/interfaces/REST/go.server/middlewares"
 	"github.com/CaliOpen/Caliopen/src/backend/main/go.main"
@@ -17,56 +18,103 @@ func SimpleSearch(ctx *gin.Context) {
 	var limit, offset int
 	user_UUID.UnmarshalBinary(user_uuid.Bytes())
 	query := ctx.Request.URL.Query()
-	// simply check if param 'context' is present.
-	// empty 'context' is allowed for now because API doesn't handle it.
-	if _, ok := query["context"]; !ok {
-		e := swgErr.New(http.StatusUnprocessableEntity, "Missing 'context' param in query")
-		http_middleware.ServeError(ctx.Writer, ctx.Request, e)
-		ctx.Abort()
-		return
+
+	// check request consistency. (see search API readme in doc folder)
+	invalid := false
+	reasons := []error{}
+	if len(query) == 0 {
+		invalid = true
+		reasons = append(reasons, errors.New("Empty query."))
+	}
+	term, term_ok := query["term"]
+	if !term_ok {
+		invalid = true
+		reasons = append(reasons, errors.New("'term' param is missing."))
+	}
+	for _, t := range term {
+		if len(t) < 3 {
+			invalid = true
+			reasons = append(reasons, errors.New("'term' param must length 3 chars at least."))
+		}
 	}
 
-	query.Del("context") // do not take into account of context for now
+	doc_type, has_doc_type := query["doctype"]
 
 	if l, ok := query["limit"]; ok {
-		limit, _ = strconv.Atoi(l[0])
-		query.Del("limit")
+		if !has_doc_type {
+			invalid = true
+			reasons = append(reasons, errors.New("'limit' param only allowed if 'doctype' param also provided."))
+		} else {
+			limit, _ = strconv.Atoi(l[0])
+		}
 	}
 	if o, ok := query["offset"]; ok {
-		offset, _ = strconv.Atoi(o[0])
-		query.Del("offset")
-	}
-	if len(query) == 0 {
-		e := swgErr.New(http.StatusUnprocessableEntity, "Missing terms in query")
-		http_middleware.ServeError(ctx.Writer, ctx.Request, e)
-		ctx.Abort()
-		return
+		if !has_doc_type {
+			invalid = true
+			reasons = append(reasons, errors.New("'offset' param only allowed if 'doctype' param also provided."))
+		} else {
+			offset, _ = strconv.Atoi(o[0])
+		}
 	}
 
-	for _, value := range query {
-		for _, str := range value {
-			if len(str) < 3 {
-				e := swgErr.New(http.StatusUnprocessableEntity, "At least one term is less than 3 chars")
-				http_middleware.ServeError(ctx.Writer, ctx.Request, e)
-				ctx.Abort()
-				return
+	// build the search object
+	search := IndexSearch{
+		User_id: user_UUID,
+		Limit:   limit,
+		Offset:  offset,
+	}
+
+	if field, ok := query["field"]; ok {
+		if len(field) > 1 {
+			invalid = true
+			reasons = append(reasons, errors.New("At most one 'field' param allowed."))
+		} else {
+			search.Terms = map[string][]string{field[0]: query["term"]} // take only first field provided for now
+		}
+	} else {
+		search.Terms = map[string][]string{"_all": query["term"]}
+	}
+
+	if has_doc_type {
+		if len(doc_type) > 1 {
+			invalid = true
+			reasons = append(reasons, errors.New("At most one 'doctype' param allowed."))
+		} else {
+			switch doc_type[0] { // take only first doctype provided for now
+			case "message":
+				search.DocType = MessageIndexType
+			case "contact":
+				search.DocType = ContactIndexType
+			default:
+				invalid = true
+				reasons = append(reasons, errors.New("'doctype' unknown."))
 			}
 		}
 	}
 
-	search := IndexSearch{
-		User_id: user_UUID,
-		Terms:   map[string][]string(query),
-		Limit:   limit,
-		Offset:  offset,
+	if invalid {
+		e := swgErr.CompositeValidationError(reasons...)
+		http_middleware.ServeError(ctx.Writer, ctx.Request, e)
+		ctx.Abort()
+		return
 	}
+
+	// trigger the search
 	result, err := caliopen.Facilities.RESTfacility.Search(search)
+
+	// handle response
 	if err != nil {
 		e := swgErr.New(http.StatusFailedDependency, err.Error())
 		http_middleware.ServeError(ctx.Writer, ctx.Request, e)
 		ctx.Abort()
 	} else {
-		ctx.Data(http.StatusOK, "application/json; charset=utf-8", result)
+		response, err := result.MarshalFrontEnd()
+		if err != nil {
+			e := swgErr.New(http.StatusFailedDependency, err.Error())
+			http_middleware.ServeError(ctx.Writer, ctx.Request, e)
+			ctx.Abort()
+		}
+		ctx.Data(http.StatusOK, "application/json; charset=utf-8", response)
 
 	}
 }
