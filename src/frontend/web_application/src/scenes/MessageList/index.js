bda9aeff0fa6b17fe6468d5394b38e6b73130e71
David Epely <esion99@gmail.com>
2017-09-29 14:54:57 +0200

    dicsussion tab should not close if there is a draft saved
    fix #507

--- src/frontend/web_application/src/scenes/MessageList/index.js
+++ src/frontend/web_application/src/scenes/MessageList/index.js
@@ -3,7 +3,7 @@ import { bindActionCreators, compose } from 'redux';
 import { connect } from 'react-redux';
 import { withTranslator } from '@gandi/react-translate';
 import { matchPath } from 'react-router-dom';
-import { requestMessages, postActions, deleteMessage, invalidate, loadMore, hasMore as getHasMore } from '../../store/modules/message';
+import { requestMessages, postActions, deleteMessage, loadMore, hasMore as getHasMore } from '../../store/modules/message';
 import { removeTab } from '../../store/modules/tab';
 import Presenter from './presenter';
 
@@ -39,23 +39,25 @@ const messageCollectionStateSelector = createSelector(
 
 const mapStateToProps = createSelector(
   [messageByIdSelector, discussionIdSelector, currentTabSelector, messageCollectionStateSelector],
-  (messagesById, discussionId, currentTab, { didInvalidate, messageIds, hasMore, isFetching }) => ({
-    discussionId,
-    didInvalidate,
-    isFetching,
-    hasMore,
-    messages: messageIds
-      .map(messageId => messagesById[messageId])
-      .filter(message => message.is_draft !== true),
-    currentTab,
-  })
+  (messagesById, discussionId, currentTab, { didInvalidate, messageIds, hasMore, isFetching }) => {
+    const messages = messageIds.map(messageId => messagesById[messageId]);
+
+    return {
+      discussionId,
+      didInvalidate,
+      isFetching,
+      hasMore,
+      messages: messages.filter(message => message.is_draft !== true),
+      hasDraft: messages.some(message => message.is_draft),
+      currentTab,
+    };
+  }
 );
 
 const mapDispatchToProps = (dispatch, ownProps) => bindActionCreators({
   requestMessages: requestMessages.bind(null, 'discussion', getDiscussionIdFromProps(ownProps)),
   loadMore: loadMore.bind(null, 'discussion', getDiscussionIdFromProps(ownProps)),
   deleteMessage,
-  invalidate: invalidate.bind(null, 'discussion', getDiscussionIdFromProps(ownProps)),
   setMessageRead: ({ message, isRead = true }) => {
     const action = isRead ? 'set_read' : 'set_unread';
 
