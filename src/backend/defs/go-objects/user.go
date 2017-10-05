95663cd96c8331d3b9933deea6b050a6b1fad07d
Stan <git-ssa@sapienssapide.com>
2017-10-05 11:47:04 +0200

    Implement unmarshalCQL for object user and fix bugs in that object.

--- src/backend/defs/go-objects/user.go
+++ src/backend/defs/go-objects/user.go
@@ -8,6 +8,7 @@ package objects
 
 import (
 	"encoding/json"
+	"github.com/gocql/gocql"
 	"github.com/satori/go.uuid"
 	"time"
 )
@@ -17,21 +18,57 @@ type User struct {
 	DateInsert       time.Time         `cql:"date_insert"              json:"date_insert"                              formatter:"RFC3339Milli"`
 	FamilyName       string            `cql:"family_name"              json:"family_name"`
 	GivenName        string            `cql:"given_name"               json:"given_name"`
-	LocalIdentities  []LocalIdentity   `cql:"local_identities"         json:"local_identities"`
+	LocalIdentities  []string          `cql:"local_identities"         json:"local_identities"`
 	MainUserId       UUID              `cql:"main_user_id"             json:"main_user_id"`
 	Name             string            `cql:"name"                     json:"name"`
 	Params           map[string]string `cql:"params"                   json:"params"`
-	Password         string            `cql:"password"                 json:"password"`
-	PrivacyIndex     *PrivacyIndex     `cql:"privacy_index"            json:"privacy_index"`
+	Password         []byte            `cql:"password"                 json:"password"`
+	PrivacyIndex     *PrivacyIndex     `cql:"pi"                       json:"pi"`
 	*PrivacyFeatures `cql:"privacy_features"         json:"privacy_features"`
-	RecoveryMail     string `cql:"recovery_mail"            json:"recovery_mail"`
+	RecoveryEmail    string `cql:"recovery_email"            json:"recovery_email"`
 	UserId           UUID   `cql:"user_id"                  json:"user_id"              elastic:"omit"      formatter:"rfc4122"`
 }
 
 // unmarshal a map[string]interface{} that must owns all Contact's fields
 // typical usage is for unmarshaling response from Cassandra backend
 func (user *User) UnmarshalCQLMap(input map[string]interface{}) {
-	//TODO
+	contactId, _ := input["contact_id"].(gocql.UUID)
+	user.ContactId.UnmarshalBinary(contactId.Bytes())
+	user.DateInsert, _ = input["date_insert"].(time.Time)
+	user.FamilyName, _ = input["family_name"].(string)
+	user.GivenName, _ = input["given_name"].(string)
+	user.LocalIdentities, _ = input["local_identities"].([]string)
+	mainUserId, _ := input["main_user_id"].(gocql.UUID)
+	user.MainUserId.UnmarshalBinary(mainUserId.Bytes())
+	user.Name, _ = input["Name"].(string)
+	password, _ := input["password"].(string)
+	user.Password = []byte(password)
+	if input["pi"] != nil {
+		i_pi, _ := input["pi"].(map[string]interface{})
+		pi := PrivacyIndex{}
+		pi.Comportment, _ = i_pi["comportment"].(int)
+		pi.Context, _ = i_pi["context"].(int)
+		pi.DateUpdate, _ = i_pi["date_update"].(time.Time)
+		pi.Technic, _ = i_pi["technic"].(int)
+		pi.Version, _ = i_pi["version"].(int)
+		user.PrivacyIndex = &pi
+	} else {
+		user.PrivacyIndex = nil
+	}
+	if input["privacy_features"] != nil {
+		i_pf, _ := input["privacy_features"].(map[string]string)
+		pf := PrivacyFeatures{}
+		for k, v := range i_pf {
+			pf[k] = v
+		}
+		user.PrivacyFeatures = &pf
+
+	} else {
+		user.PrivacyFeatures = nil
+	}
+	user.RecoveryEmail, _ = input["recovery_email"].(string)
+	userid, _ := input["user_id"].(gocql.UUID)
+	user.UserId.UnmarshalBinary(userid.Bytes())
 }
 
 func (user *User) UnmarshalJSON(b []byte) error {
@@ -57,15 +94,7 @@ func (user *User) UnmarshalMap(input map[string]interface{}) error {
 
 	user.FamilyName, _ = input["family_name"].(string)
 	user.GivenName, _ = input["given_name"].(string)
-
-	if identities, ok := input["local_identities"]; ok {
-		for _, identity := range identities.([]interface{}) {
-			I := new(LocalIdentity)
-			if err := I.UnmarshalMap(identity.(map[string]interface{})); err == nil {
-				user.LocalIdentities = append(user.LocalIdentities, *I)
-			}
-		}
-	}
+	user.LocalIdentities, _ = input["local_identities"].([]string)
 
 	if main_user_id, ok := input["main_user_id"].(string); ok {
 		if id, err := uuid.FromString(main_user_id); err == nil {
@@ -75,9 +104,10 @@ func (user *User) UnmarshalMap(input map[string]interface{}) error {
 
 	user.Name, _ = input["name"].(string)
 	user.Params, _ = input["params"].(map[string]string)
-	user.Password, _ = input["password"].(string)
+	password, _ := input["password"].(string)
+	user.Password = []byte(password)
 
-	if pi, ok := input["privacy_index"]; ok {
+	if pi, ok := input["pi"]; ok {
 		PI := new(PrivacyIndex)
 		if err := PI.UnmarshalMap(pi.(map[string]interface{})); err == nil {
 			user.PrivacyIndex = PI
@@ -90,7 +120,7 @@ func (user *User) UnmarshalMap(input map[string]interface{}) error {
 		user.PrivacyFeatures = PF
 	}
 
-	user.RecoveryMail, _ = input["recovery_mail"].(string)
+	user.RecoveryEmail, _ = input["recovery_email"].(string)
 
 	if user_id, ok := input["user_id"].(string); ok {
 		if id, err := uuid.FromString(user_id); err == nil {
@@ -127,8 +157,6 @@ func (user *User) JsonTags() (tags map[string]string) {
 
 func (user *User) NewEmpty() interface{} {
 	u := new(User)
-	u.LocalIdentities = []LocalIdentity{}
 	u.Params = make(map[string]string)
-	u.PrivacyIndex = &PrivacyIndex{}
 	return u
 }
