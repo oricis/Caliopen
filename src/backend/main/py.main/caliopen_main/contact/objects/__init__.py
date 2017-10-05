4f9593ace8f95d5bd3679c61fe9257eb702d0d36
Aymeric Barantal <mric@gandi.net>
2017-10-05 17:23:05 +0200

    Please put primary key not in alphabetical order !!!!

---
+++ src/backend/main/py.main/caliopen_main/contact/objects/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Caliopen contact classes."""
+from __future__ import absolute_import, print_function, unicode_literals
+
+from .contact import Contact, PublicKey
+
+__all__ = ['Contact', 'PublicKey']
