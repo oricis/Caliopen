0db0b0bac2c498896a9cec57f1e59979626eb37f
Aymeric Barantal <mric@gandi.net>
2017-10-05 12:56:55 +0200

    Add a qualifiers namespace to prepare for more than only message one

---
+++ src/backend/components/py.pi/caliopen_pi/qualifiers/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Caliopen user message qualification logic."""
+from __future__ import absolute_import, print_function, unicode_literals
+
+from .mail import UserMessageQualifier
+
+__all__ = ['UserMessageQualifier']
