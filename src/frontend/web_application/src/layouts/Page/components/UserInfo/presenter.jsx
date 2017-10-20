796bfb6d59e46b3e6a1b0e227ad9983c17e8dbb3
Mylene <mylene.bressan@gandi.net>
2017-10-20 12:32:58 +0200

    Fix avatar in usermenu

--- src/frontend/web_application/src/layouts/Page/components/UserInfo/presenter.jsx
+++ src/frontend/web_application/src/layouts/Page/components/UserInfo/presenter.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-// import ContactAvatarLetter from '../../../../components/ContactAvatarLetter';
+import ContactAvatarLetter from '../../../../components/ContactAvatarLetter';
 import './style.scss';
 
 const UserInfo = ({ user, className }) => (
   <div className={classnames('m-user-info', className)}>
-    {/* user && user.contact && (
+    {user && user.contact && (
       <div className="m-user-info__avatar">
         <ContactAvatarLetter contact={user.contact} modifiers={{ size: 'small' }} />
       </div>
-    ) */}
+    )}
     <div className="m-user-info__username">{user.name}</div>
   </div>
 );
