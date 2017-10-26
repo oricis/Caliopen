9d39db33bcd82a5247c7f4dfb3587d47786002ca
Aymeric Barantal <mric@gandi.net>
2017-10-26 14:29:51 +0200

    Export ReservedName in core.user namespace

--- src/backend/main/py.main/caliopen_main/user/core/__init__.py
+++ src/backend/main/py.main/caliopen_main/user/core/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-from .user import User, Tag, FilterRule, RemoteIdentity
+from .user import User, Tag, FilterRule, RemoteIdentity, ReservedName
 from .device import Device, DevicePublicKey
 
 
 __all__ = [
-    'User', 'Tag', 'FilterRule', 'RemoteIdentity',
+    'User', 'Tag', 'FilterRule', 'RemoteIdentity', 'ReservedName'
     'Device', 'DevicePublicKey',
 ]
