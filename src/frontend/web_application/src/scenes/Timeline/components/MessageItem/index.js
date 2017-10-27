cc41801f4611a156741c7d963a2c36f9f72d1d19
David Epely <esion99@gmail.com>
2017-10-27 12:49:09 +0200

    consistent usage of react-moment in Timeline
    + select locale from settings in Timeline & discussion
    
    fix #567

--- src/frontend/web_application/src/scenes/Timeline/components/MessageItem/index.js
+++ src/frontend/web_application/src/scenes/Timeline/components/MessageItem/index.js
@@ -1,4 +1,9 @@
+import { compose } from 'redux';
 import { withTranslator } from '@gandi/react-translate';
+import { withSettings } from '../../../../services/settings';
 import Presenter from './presenter';
 
-export default withTranslator()(Presenter);
+export default compose(
+  withSettings(),
+  withTranslator()
+)(Presenter);
