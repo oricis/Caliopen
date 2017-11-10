aa6e8e3d5af5089c779f140808734b5f692b680a
David Epely <esion99@gmail.com>
2017-11-10 10:00:37 +0100

    fix search functional test
    and use only CrappyURLSearchParams

--- src/frontend/web_application/src/services/url/index.js
+++ src/frontend/web_application/src/services/url/index.js
@@ -1,11 +1,3 @@
 import CrappyURLSearchParams from './CrappyURLSearchParams';
 
-let nodeURLSearchParams;
-
-if (BUILD_TARGET === 'server') {
-  // eslint-disable-next-line global-require
-  nodeURLSearchParams = require('url').URLSearchParams;
-}
-
-export const URLSearchParams = global.URLSearchParams ||
-  nodeURLSearchParams || CrappyURLSearchParams;
+export const URLSearchParams = CrappyURLSearchParams;
