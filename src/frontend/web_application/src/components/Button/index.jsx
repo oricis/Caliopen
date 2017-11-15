8d4124776521b88d7aa067c6f836f1f1a2cbe6f3
David Epely <esion99@gmail.com>
2017-11-15 17:02:18 +0100

    refactor Button & RawButton & support component as icon

--- src/frontend/web_application/src/components/Button/index.jsx
+++ src/frontend/web_application/src/components/Button/index.jsx
@@ -1,91 +1,90 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Icon from '../Icon';
-
+import RawButton from '../RawButton';
 import './style.scss';
 
-export const RawButton = ({ children, type, ...props }) => {
-  const buttonProps = {
-    ...props,
-    type,
+class Button extends PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+    shape: PropTypes.oneOf(['plain', 'hollow']),
+    icon: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    display: PropTypes.oneOf(['inline', 'inline-block', 'expanded']),
+    color: PropTypes.oneOf(['success', 'alert', 'secondary', 'active']),
+    responsive: PropTypes.oneOf(['icon-only', 'text-only']),
+    accessKey: PropTypes.string,
+    disabled: PropTypes.bool,
+  };
+  static defaultProps = {
+    className: undefined,
+    children: null,
+    shape: null,
+    icon: null,
+    display: 'inline-block', // default style for button is --inline-block
+    color: null,
+    responsive: null,
+    accessKey: null,
+    disabled: false,
   };
 
-  return <button {...buttonProps}>{children}</button>;
-};
+  renderIcon({ className }) {
+    const { icon } = this.props;
+    if (React.isValidElement(icon)) {
+      const iconProps = {
+        ...icon.props,
+        className: classnames(icon.props.className, className),
+      };
 
-RawButton.propTypes = {
-  type: PropTypes.oneOf(['button', 'submit']),
-  children: PropTypes.node.isRequired,
-};
+      return (<icon.type {...iconProps} />);
+    }
 
-RawButton.defaultProps = {
-  type: 'button',
-};
-
-const Button = ({
-  children, className, icon, display, color, shape, responsive, ...props
-}) => {
-  const buttonProps = {
-    ...props,
-    className: classnames(
-      className,
-      'm-button',
-      {
-        'm-button--active': color === 'active',
-        'm-button--alert': color === 'alert',
-        'm-button--secondary': color === 'secondary',
-        'm-button--success': color === 'success',
-
-        'm-button--expanded': display === 'expanded',
-        'm-button--inline': display === 'inline',
-        'm-button--inline-block': display === 'inline-block',
-
-        'm-button--plain': shape === 'plain',
-        'm-button--hollow': shape === 'hollow',
-
-        'm-button--icon': icon,
-
-        'm-button--icon-only': responsive === 'icon-only',
-        'm-button--text-only': responsive === 'text-only',
-      }
-    ),
-  };
-
-  if (icon) {
-    return (
-      <RawButton {...buttonProps}>
-        <Icon className="m-button__icon" type={icon} />
-        {children && <span className="m-button__text">{children}</span>}
-      </RawButton>
-    );
+    return (<Icon className={className} type={icon} />);
   }
 
-  return <RawButton {...buttonProps}>{children}</RawButton>;
-};
+  render() {
+    const {
+      children, className, icon, display, color, shape, responsive, ...props
+    } = this.props;
 
-Button.propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node,
-  shape: PropTypes.oneOf(['plain', 'hollow']),
-  icon: PropTypes.string,
-  display: PropTypes.oneOf(['inline', 'inline-block', 'expanded']),
-  color: PropTypes.oneOf(['success', 'alert', 'secondary', 'active']),
-  responsive: PropTypes.oneOf(['icon-only', 'text-only']),
-  accessKey: PropTypes.string,
-  disabled: PropTypes.bool,
-};
+    const buttonProps = {
+      ...props,
+      className: classnames(
+        className,
+        'm-button',
+        {
+          'm-button--active': color === 'active',
+          'm-button--alert': color === 'alert',
+          'm-button--secondary': color === 'secondary',
+          'm-button--success': color === 'success',
 
-Button.defaultProps = {
-  className: undefined,
-  children: null,
-  shape: null,
-  icon: null,
-  display: 'inline-block', // default style for button is --inline-block
-  color: null,
-  responsive: null,
-  accessKey: null,
-  disabled: false,
-};
+          'm-button--expanded': display === 'expanded',
+          'm-button--inline': display === 'inline',
+          'm-button--inline-block': display === 'inline-block',
+
+          'm-button--plain': shape === 'plain',
+          'm-button--hollow': shape === 'hollow',
+
+          'm-button--icon': icon,
+
+          'm-button--icon-only': responsive === 'icon-only',
+          'm-button--text-only': responsive === 'text-only',
+        }
+      ),
+    };
+
+    if (icon) {
+      return (
+        <RawButton {...buttonProps}>
+          {this.renderIcon({ icon, className: 'm-button__icon' })}
+          {children && <span className="m-button__text">{children}</span>}
+        </RawButton>
+      );
+    }
+
+    return <RawButton {...buttonProps}>{children}</RawButton>;
+  }
+}
 
 export default Button;
