c7ca393f80398db920a2832a9d5f0f2726150313
stan <stan@sapienssapide.fr>
2018-01-05 11:56:07 +0100

    Add HasNested, HasRelated, HasLookup and MarshalNested interfaces defs.

--- src/backend/defs/go-objects/common.go
+++ src/backend/defs/go-objects/common.go
@@ -4,7 +4,9 @@
 
 package objects
 
-import "gopkg.in/oleiade/reflections.v1"
+import (
+	"gopkg.in/oleiade/reflections.v1"
+)
 
 // this interface is mainly a collection of helpers to facilitate the job of reflection
 type CaliopenObject interface {
@@ -17,7 +19,31 @@ type CaliopenObject interface {
 	UnmarshalJSON(b []byte) error
 }
 
-// generic implementation of the jsonTags func for CaliopenObject interface
+// types below are helpers to manage embedded and related struct within storage
+// as well as lookup tables.
+type (
+	// objects with embedded struct that must be stored alongside their parent.
+	// these embedded struct must have an
+	HasNested interface {
+		GetSetNested() <-chan interface{}
+	}
+
+	// objects with joined objects stored in separate table
+	HasRelated interface {
+		GetRelated() map[string]interface{}
+	}
+
+	// objects with related tables that must be maintained up-to-date
+	HasLookup interface {
+		GetLookup() map[string]interface{}
+	}
+
+	NewMarshaller interface {
+		MarshallNew() // ensure that mandatory properties are correctly set on new structs.
+	}
+)
+
+// generic implementation of the jsonTags func (see above) for CaliopenObject interface
 func jsonTags(obj interface{}) (tags map[string]string) {
 	tags = make(map[string]string)
 	fields, err := reflections.Fields(obj)
@@ -31,4 +57,13 @@ func jsonTags(obj interface{}) (tags map[string]string) {
 		}
 	}
 	return
+}
+
+// MarshalNested calls MarshalNew() on embedded structs
+func MarshalNested(obj HasNested) {
+	for nested := range obj.GetSetNested() {
+		if nest, ok := nested.(NewMarshaller); ok {
+			nest.MarshallNew()
+		}
+	}
 }
