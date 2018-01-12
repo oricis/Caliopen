542effe2ec48390d9a59f63663105ef7e9839661
David Epely <esion99@gmail.com>
2018-01-12 10:36:21 +0100

    following last spec: only label is allowed on tag creation
    cf #753

--- src/frontend/web_application/src/modules/tags/actions/createTag/index.js
+++ src/frontend/web_application/src/modules/tags/actions/createTag/index.js
@@ -1,11 +1,4 @@
 import { createTag as createTagBase } from '../../../../store/modules/tag';
 
-export const createTag = tag => (dispatch) => {
-  const data = {
-    ...tag,
-    name: tag.name || tag.label.toLowerCase(),
-  };
-
-  return dispatch(createTagBase({ tag: data }))
-    .then(response => response.payload.data);
-};
+export const createTag = tag => dispatch => dispatch(createTagBase({ tag }))
+  .then(response => response.payload.data);
