6d079b3c175dea022b2e0fca76a9d86c3646f3f2
Mylene <mylene.bressan@gandi.net>
2018-01-16 15:35:42 +0100

    Use AuthorAvatar as label for checkbox - add isSelected prop to IconLetter

--- src/frontend/web_application/src/components/IconLetter/index.jsx
+++ src/frontend/web_application/src/components/IconLetter/index.jsx
@@ -4,18 +4,20 @@ import classnames from 'classnames';
 import getClassName from './services/stylesheet-helper';
 import './style.scss';
 
-const IconLetter = ({ word, className }) => (
-  <span className={classnames(className, getClassName(word))} />
+const IconLetter = ({ word, className, isSelected }) => (
+  <span className={classnames(className, isSelected ? 'm-letter--checked' : getClassName(word))} />
 );
 
 IconLetter.propTypes = {
   word: PropTypes.string,
   className: PropTypes.string,
+  isSelected: PropTypes.bool,
 };
 
 IconLetter.defaultProps = {
   word: null,
   className: null,
+  isSelected: false,
 };
 
 export default IconLetter;
