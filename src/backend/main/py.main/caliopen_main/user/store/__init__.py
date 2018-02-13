2564c2d39e7d1148f8aecd300135e588f71fb249
Aymeric Barantal <mric@gandi.net>
2018-02-13 15:43:33 +0100

    Fix old Device location

--- src/backend/main/py.main/caliopen_main/user/store/__init__.py
+++ src/backend/main/py.main/caliopen_main/user/store/__init__.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import, print_function, unicode_literals
 from .user import User, UserName, ReservedName, FilterRule, UserRecoveryEmail
 from .user import RemoteIdentity, IndexUser, Settings
 from .tag import UserTag
-from .device import Device, DeviceLocation, DeviceConnectionLog
 from .local_identity_index import IndexedLocalIdentity
 from .local_identity import LocalIdentity
 
@@ -13,6 +12,5 @@ __all__ = [
     'User', 'UserName', 'UserRecoveryEmail', 'UserTag', 'FilterRule',
     'ReservedName',
     'RemoteIdentity', 'IndexUser', 'UserTag', 'Settings',
-    'Device', 'DeviceLocation',
-    'DeviceConnectionLog', 'IndexedLocalIdentity', 'LocalIdentity',
+    'IndexedLocalIdentity', 'LocalIdentity',
 ]
