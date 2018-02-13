9aed64d9af34f6087c2280119374378f44fdddd2
Aymeric Barantal <mric@gandi.net>
2018-02-13 16:09:10 +0100

    Bad Device namespace remaining

--- src/backend/main/py.main/caliopen_main/user/core/__init__.py
+++ src/backend/main/py.main/caliopen_main/user/core/__init__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-from caliopen_main.device.core import Device
 from .user import User, Tag, FilterRule, RemoteIdentity, ReservedName
 
 __all__ = [
-    'User', 'Tag', 'FilterRule', 'RemoteIdentity',
-    'ReservedName', 'Device'
+    'User', 'Tag', 'FilterRule', 'RemoteIdentity', 'ReservedName',
 ]
