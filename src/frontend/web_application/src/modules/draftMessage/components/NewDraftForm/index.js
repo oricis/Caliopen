fbe1bcd38752261bdfdfb34dac460f19155c83b1
Mylene <mylene.bressan@gandi.net>
2018-02-16 13:58:09 +0100

    Connect user in NewDraftForm to render ContactAvatarLetter

--- src/frontend/web_application/src/modules/draftMessage/components/NewDraftForm/index.js
+++ src/frontend/web_application/src/modules/draftMessage/components/NewDraftForm/index.js
@@ -1,4 +1,18 @@
 import { withI18n } from 'lingui-react';
+import { createSelector } from 'reselect';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import Presenter from './presenter';
 
-export default withI18n()(Presenter);
+const userSelector = state => state.user.user;
+const mapStateToProps = createSelector(
+  [userSelector],
+  user => ({
+    user,
+  })
+);
+
+export default compose(
+  withI18n(),
+  connect(mapStateToProps),
+)(Presenter);
