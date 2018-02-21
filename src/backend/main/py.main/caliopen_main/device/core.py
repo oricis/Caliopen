0b3ca497b4aaba2f21d1510d66b92bdee435f144
Aymeric Barantal <mric@gandi.net>
2018-02-21 16:54:38 +0100

    Move logic to create a default device in core Device class

--- src/backend/main/py.main/caliopen_main/device/core.py
+++ src/backend/main/py.main/caliopen_main/device/core.py
@@ -2,6 +2,7 @@
 """Caliopen device core classes."""
 from __future__ import absolute_import, print_function, unicode_literals
 import logging
+import uuid
 
 from .store import Device as ModelDevice
 from .store import DeviceLocation as ModelDeviceLocation
@@ -9,6 +10,9 @@ from .store import DeviceLocation as ModelDeviceLocation
 from caliopen_storage.core import BaseUserCore
 from caliopen_storage.core.mixin import MixinCoreRelation, MixinCoreNested
 from caliopen_main.common.core import PublicKey, BaseUserRelatedCore
+from caliopen_main.common.parameters import NewPublicKey
+from caliopen_main.device.parameters import NewDevice
+from caliopen_pi.qualifiers import NewDeviceQualifier
 
 
 log = logging.getLogger(__name__)
@@ -31,6 +35,37 @@ class Device(BaseUserCore, MixinCoreRelation, MixinCoreNested):
         'public_keys': PublicKey,
         'locations': ModelDeviceLocation,
     }
+
+    @classmethod
+    def create_default(cls, user, param, headers):
+        """Create the default device on signup."""
+        dev = NewDevice()
+        dev.name = 'default'
+        dev.device_id = param['device_id']
+        dev.user_agent = headers.get('User-Agent')
+        dev.ip_creation = headers.get('X-Forwarded-For')
+        qualifier = NewDeviceQualifier(user)
+        qualifier.process(dev)
+        dev.type = dev.privacy_features.get('device_type', 'other')
+        # Device ecdsa key
+        dev_key = NewPublicKey()
+        dev_key.key_id = uuid.uuid4()
+        dev_key.resource_id = dev.device_id
+        dev_key.resource_type = 'device'
+        dev_key.label = 'ecdsa key'
+        dev_key.kty = 'ec'
+        dev_key.use = 'sig'
+        dev_key.x = int(param['ecdsa_key']['x'], 16)
+        dev_key.y = int(param['ecdsa_key']['y'], 16)
+        dev_key.crv = param['ecdsa_key']['curve']
+        # XXX Should be better design
+        alg_map = {
+            'P-256': 'ES256',
+            'P-384': 'ES384',
+            'P-521': 'ES512'
+        }
+        dev_key.alg = alg_map[dev_key.crv]
+        return Device.create(user, dev, public_keys=[dev_key])
 
     @classmethod
     def create(cls, user, device, **related):
