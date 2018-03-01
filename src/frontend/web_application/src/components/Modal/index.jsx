110a7d5dc2faf98d3bbd34e292b8c0dda1732275
Mylene <mylene.bressan@gandi.net>
2018-03-01 11:31:59 +0100

    set #root as appElement

--- src/frontend/web_application/src/components/Modal/index.jsx
+++ src/frontend/web_application/src/components/Modal/index.jsx
@@ -1,41 +1,48 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import ReactModal from 'react-modal';
 import classnames from 'classnames';
 import Button from '../Button';
 import './style.scss';
 
-const Modal = ({ className, title, children, onClose, ...props }) => (
-  <ReactModal
-    className={classnames('m-modal', className)}
-    overlayClassName="m-modal__overlay"
-    appElement={document.body}
-    {...props}
-  >
-    <header className="m-modal__header">
-      {title && (
-        <div className="m-modal__title">{title}</div>
-      )}
-      <Button className="m-modal__close" onClick={onClose} icon="remove" />
-    </header>
-    <div className="m-modal__content">
-      {children}
-    </div>
-  </ReactModal>
-);
+class Modal extends PureComponent {
+  static propTypes = {
+    className: PropTypes.string,
+    title: PropTypes.node,
+    children: PropTypes.node,
+    onClose: PropTypes.func,
+  };
 
-Modal.propTypes = {
-  className: PropTypes.string,
-  title: PropTypes.node,
-  children: PropTypes.node,
-  onClose: PropTypes.func,
-};
+  static defaultProps = {
+    className: undefined,
+    title: undefined,
+    children: undefined,
+    onClose: () => {},
+  };
 
-Modal.defaultProps = {
-  className: undefined,
-  title: undefined,
-  children: undefined,
-  onClose: () => {},
-};
+  render() {
+    const { className, title, children, onClose, ...props } = this.props;
+    const rootElement = document.getElementById('root');
+
+    return (
+      <ReactModal
+        className={classnames('m-modal', className)}
+        overlayClassName="m-modal__overlay"
+        appElement={rootElement}
+        {...props}
+      >
+        <header className="m-modal__header">
+          {title && (
+            <div className="m-modal__title">{title}</div>
+          )}
+          <Button className="m-modal__close" onClick={onClose} icon="remove" />
+        </header>
+        <div className="m-modal__content">
+          {children}
+        </div>
+      </ReactModal>
+    );
+  }
+}
 
 export default Modal;
