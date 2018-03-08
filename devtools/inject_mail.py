dd6f50dd8578cea97a7a1e4ff38340c47859bbb5
stan <stan@sapienssapide.fr>
2018-03-08 14:57:34 +0100

    Improve `inject_mail.py` script and add email bodies into fixtures folder.

--- devtools/inject_mail.py
+++ devtools/inject_mail.py
@@ -14,11 +14,18 @@ import argparse
 import smtplib
 
 
-def send_fake_mail(fromaddr, toaddrs, file, host, port):
+def send_fake_mail(fromaddr, toaddrs, file, subject, content_type, host, port):
+    # add useful headers. NB: do not add leading spaces in header lines
+    msg = (
+        '''MIME-Version: 1.0
+        From: {}
+        To: {}
+        Subject: {}
+        Content-Type: {}; charset=UTF-8
+        Content-Transfer-Encoding: quoted-printable
+        
+        '''.format(fromaddr, ", ".join(toaddrs), subject, content_type))
 
-    # Add the From: and To: headers at the start!
-    msg = ("From: %s\r\nTo: %s\r\n\r\n"
-           % (fromaddr, ", ".join(toaddrs)))
     with open(file) as f:
         mail = f.read()
         msg = msg.encode('ascii') + mail
@@ -33,10 +40,13 @@ if __name__ == '__main__':
     args = sys.argv
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', dest='from_')
-    parser.add_argument('-t', dest='to')
+    parser.add_argument('-t', dest='to', default='dev@caliopen.local')
+    parser.add_argument('-s', dest='subject',
+                        default='mail from inject_mail.py')
+    parser.add_argument('-c', dest='content_type', default='text/plain')
     parser.add_argument('-m', dest='mail')
-    parser.add_argument('-s', dest='host', default='localhost')
-    parser.add_argument('-p', dest='port', default=4025)
+    parser.add_argument('-o', dest='host', default='localhost')
+    parser.add_argument('-p', dest='port', default=2525)
     opts = parser.parse_args(args[1:])
-    send_fake_mail(opts.from_, [opts.to], opts.mail,
-                   opts.host, opts.port)
+    send_fake_mail(opts.from_, [opts.to], opts.mail, opts.subject,
+                   opts.content_type, opts.host, opts.port)
