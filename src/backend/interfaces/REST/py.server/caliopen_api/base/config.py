dc882b0bcddf65cf6a12630bd1a895b0b45fc2c6
Aymeric Barantal <mric@gandi.net>
2018-03-16 16:19:47 +0100

    Swagger RequestValidationError should be json formatted as it can be

--- src/backend/interfaces/REST/py.server/caliopen_api/base/config.py
+++ src/backend/interfaces/REST/py.server/caliopen_api/base/config.py
@@ -10,6 +10,22 @@ from .deserializer import json_deserializer
 
 log = logging.getLogger(__name__)
 
+"""
+Solution from :
+
+https://github.com/striglia/pyramid_swagger/issues/177#issuecomment-373220674
+
+"""
+
+
+def swagger_error_view(exc, request):
+    """Format swagger validation error."""
+    for_json = {
+        'message': exc.child.message,
+    }
+
+    return for_json
+
 
 def includeme(config):
     """Configure REST API."""
@@ -26,5 +42,9 @@ def includeme(config):
     config.scan('caliopen_api.base.errors')
     config.add_cornice_deserializer('application/json',
                                     json_deserializer)
+    swagger_context = 'pyramid_swagger.exceptions.RequestValidationError'
+    config.add_exception_view(context=swagger_context,
+                              view=swagger_error_view,
+                              renderer='json')
     config.commit()
     log.info('Base API configured')
