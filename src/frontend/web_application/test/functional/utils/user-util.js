686bed206a46700928dea5a489d7720e18794c2e
David Epely <esion99@gmail.com>
2018-03-22 21:30:21 +0100

    refactor test func signin utils method

--- src/frontend/web_application/test/functional/utils/user-util.js
+++ src/frontend/web_application/test/functional/utils/user-util.js
@@ -4,16 +4,15 @@ module.exports = {
   signin: (login, password) => {
     const loginKeys = login || 'dev';
     const passwordKeys = password || '123456';
-    browser.get('/auth/signin');
-    element(by.css('input[name=username]')).sendKeys(loginKeys);
-    element(by.css('input[name=password]')).sendKeys(passwordKeys);
-    element(by.cssContainingText('button[type=submit]', 'I\'m in a safe place')).click();
 
-    return browser.wait(EC.not(EC.urlContains('auth/signin')), 5 * 1000);
+    return browser.get('/auth/signin')
+      .then(() => element(by.css('input[name=username]')).sendKeys(loginKeys))
+      .then(() => element(by.css('input[name=password]')).sendKeys(passwordKeys))
+      .then(() => element(by.cssContainingText('button[type=submit]', 'I\'m in a safe place')).click())
+      .then(() => browser.wait(EC.not(EC.urlContains('auth/signin')), 5 * 1000))
+    ;
   },
-  signout: () => {
-    browser.get('/auth/signout');
-  },
+  signout: () => browser.get('/auth/signout'),
   showAccount: () => {
     const userMenu = element(by.css('.l-header__user'));
     userMenu.element(by.css('[data-toggle="co-user-menu"]')).click();
