bde4ce380c4e09b6475b0abf23e14081c600660f
Mylene <mylene.bressan@gandi.net>
2018-04-16 17:03:53 +0200

    Background component

--- src/frontend/web_application/src/layouts/Page/presenter.jsx
+++ src/frontend/web_application/src/layouts/Page/presenter.jsx
@@ -6,6 +6,7 @@ import Navigation from './components/Navigation';
 import NavigationAlt from './components/NavigationAlt';
 import NotificationCenter from './components/NotificationCenter';
 import CallToAction from './components/CallToAction';
+import BackgroundImage from './components/BackgroundImage';
 
 import './style.scss';
 
@@ -22,24 +23,27 @@ class PageContainer extends Component {
 
   render() {
     const { children, i18n } = this.props;
+    const fakeContext = 'secure';
 
     return (
       <OffCanvas leftChildren={<NavigationAlt />}>
-        <div className="l-body">
-          <Header />
-          <Navigation />
-          <section role="main">
-            <div className="l-body__content">{children}</div>
-          </section>
-          <section>
-            <div
-              className="l-body__footer-alpha"
-              dangerouslySetInnerHTML={{ __html: i18n._('alpha.footer.feedback', { defaults: 'Tell us if something went wrong at <a href="https://feedback.caliopen.org/">https://feedback.caliopen.org/</a>.' }) }}
-            />
-          </section>
-          <CallToAction />
-          <NotificationCenter />
-        </div>
+        <BackgroundImage context={fakeContext}>
+          <div className="l-body">
+            <Header />
+            <Navigation />
+            <section role="main">
+              <div className="l-body__content">{children}</div>
+            </section>
+            <section>
+              <div
+                className="l-body__footer-alpha"
+                dangerouslySetInnerHTML={{ __html: i18n._('alpha.footer.feedback', { defaults: 'Tell us if something went wrong at <a href="https://feedback.caliopen.org/">https://feedback.caliopen.org/</a>.' }) }}
+              />
+            </section>
+            <CallToAction />
+            <NotificationCenter />
+          </div>
+        </BackgroundImage>
       </OffCanvas>
     );
   }
