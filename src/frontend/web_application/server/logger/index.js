420735a049568dfd32f82cb0bfc1884153a767b9
David Epely <esion99@gmail.com>
2018-05-18 12:08:42 +0200

    WIP: add support of Syslog transport

--- src/frontend/web_application/server/logger/index.js
+++ src/frontend/web_application/server/logger/index.js
@@ -1,26 +1,39 @@
 const winston = require('winston');
+const { Syslog } = require('winston-syslog');
 
 const LOGGER_CATEGORY = 'caliopen-frontend';
+const stdFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp(),
+  winston.format.label({ label: LOGGER_CATEGORY }),
+  winston.format.align(),
+  winston.format.printf((info) => {
+    // console.log('info log', info);
+
+    return `${info.timestamp} ${info.level} ${info.label} - ${info.message}`
+  }),
+);
 
 let logger;
 
 const getLogger = () => {
   if (!logger) {
     logger = winston.createLogger({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.timestamp(),
-        winston.format.label({ label: LOGGER_CATEGORY }),
-        winston.format.align(),
-        winston.format.printf(info => `${info.timestamp} ${info.level} ${info.label} - ${info.message}`),
-      ),
       transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({
-          format: winston.format.uncolorize(),
-          filename: 'combined.log',
+        new winston.transports.Console({
+          format: stdFormat,
+        }),
+        // new winston.transports.File({
+        //   format: winston.format.uncolorize(),
+        //   filename: 'combined.log',
+        // }),
+        new Syslog({
+          eol: '\n',
+          facility: 'user',
+          type: '5424',
         }),
       ],
+      levels: winston.config.syslog.levels,
     });
   }
 
