453b76f5a70941cb04116712944c2657ee2e1eef
David Epely <esion99@gmail.com>
2018-05-23 22:11:51 +0200

    replace postActionHandler middleware by an action to mark message as read
    + remove invalidate discussion

--- src/frontend/web_application/src/scenes/MessageList/index.js
+++ src/frontend/web_application/src/scenes/MessageList/index.js
@@ -5,10 +5,11 @@ import { withI18n } from 'lingui-react';
 import { push } from 'react-router-redux';
 import { createNotification, NOTIFICATION_TYPE_INFO } from 'react-redux-notify';
 import { createMessageCollectionStateSelector } from '../../store/selectors/message';
-import { requestMessages, postActions, deleteMessage, loadMore, replyToMessage } from '../../store/modules/message';
+import { requestMessages, deleteMessage, loadMore, replyToMessage } from '../../store/modules/message';
 import { removeTab, updateTab } from '../../store/modules/tab';
 import { clearDraft } from '../../store/modules/draft-message';
 import { updateTagCollection, withTags } from '../../modules/tags';
+import { setMessageRead } from '../../modules/message';
 import { withCurrentTab } from '../../hoc/tab';
 import Presenter from './presenter';
 
@@ -64,11 +65,7 @@ const mapDispatchToProps = (dispatch, ownProps) => bindActionCreators({
   requestMessages: requestMessages.bind(null, 'discussion', getDiscussionIdFromProps(ownProps)),
   loadMore: loadMore.bind(null, 'discussion', getDiscussionIdFromProps(ownProps)),
   deleteMessage: onDeleteMessage,
-  setMessageRead: ({ message, isRead = true }) => {
-    const action = isRead ? 'set_read' : 'set_unread';
-
-    return postActions({ message, actions: [action] });
-  },
+  setMessageRead,
   removeTab,
   updateTab,
   replyToMessage,
--- src/frontend/web_application/src/store/middlewares/messages-middleware.js
+++ src/frontend/web_application/src/store/middlewares/messages-middleware.js
@@ -1,4 +1,4 @@
-import { POST_ACTIONS_SUCCESS, DELETE_MESSAGE_SUCCESS, LOAD_MORE_MESSAGES, requestMessage, requestMessages, invalidate, getNextOffset } from '../modules/message';
+import { DELETE_MESSAGE_SUCCESS, LOAD_MORE_MESSAGES, requestMessages, invalidate, getNextOffset } from '../modules/message';
 
 const getListOfCollectionsToInvalidate = (messagesCollections, message) => [
   ...new Set(Object.keys(messagesCollections).reduce((acc, type) => [
@@ -17,20 +17,6 @@ const getListOfCollectionsToInvalidate = (messagesCollections, message) => [
     }, []),
   ], [])),
 ];
-
-const postActionsHandler = ({ store, action }) => {
-  if (action.type !== POST_ACTIONS_SUCCESS) {
-    return;
-  }
-
-  const { meta: { previousAction: { payload: { message } } } } = action;
-
-  const state = store.getState();
-  getListOfCollectionsToInvalidate(state.message.messagesCollections, message)
-    .forEach(({ type, key }) => store.dispatch(invalidate(type, key)));
-
-  store.dispatch(requestMessage(message.message_id));
-};
 
 const deleteHandler = ({ store, action }) => {
   if (action.type !== DELETE_MESSAGE_SUCCESS) {
@@ -58,7 +44,6 @@ const loadMoreHandler = ({ store, action }) => {
 export default store => next => (action) => {
   const result = next(action);
 
-  postActionsHandler({ store, action });
   deleteHandler({ store, action });
   loadMoreHandler({ store, action });
 
