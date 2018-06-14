9936c10a37383e776c1972d036dac21754ebcdd4
Aymeric Barantal <mric@gandi.net>
2018-06-14 16:58:33 +0200

    Enable HKPDiscovery discoverer in PublicKeyDiscoverer

--- src/backend/components/py.pgp/caliopen_pgp/keys/discoverer.py
+++ src/backend/components/py.pgp/caliopen_pgp/keys/discoverer.py
@@ -6,6 +6,7 @@ import logging
 
 from .keybase import KeybaseDiscovery
 from .rfc7929 import DNSDiscovery
+from .hkp import HKPDiscovery
 
 log = logging.getLogger(__name__)
 
@@ -24,15 +25,20 @@ class PublicKeyDiscoverer(object):
         if params.get('enable'):
             disco = KeybaseDiscovery(params)
             self.discoverers['keybase'] = disco
+        params = conf.get('key_discovery', {}).get('hkp', {})
+        if params.get('enable'):
+            disco = HKPDiscovery(params)
+            self.discoverers['hkp'] = disco
 
     def search_email(self, email):
         """Search for an email in all discoveries service configured."""
         found_keys = []
         for disco in self.discoverers:
-            try:
-                key = self.discoverers[disco].find_by_email(email)
-                found_keys.append(key)
-            except Exception as exc:
-                log.error('Exception during key lookup using {0} '
-                          'for email {1}: {2}'.format(disco, email, exc))
+            if hasattr(self.discoverers[disco], 'find_by_email'):
+                try:
+                    key = self.discoverers[disco].find_by_email(email)
+                    found_keys.append(key)
+                except Exception as exc:
+                    log.error('Exception during key lookup using {0} '
+                              'for email {1}: {2}'.format(disco, email, exc))
         return found_keys
