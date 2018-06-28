0396e6e247f1bbd68defe99ec976ea0c5216722f
Pablo <ppiedrafita95@gmail.com>
2018-06-28 14:33:19 +0200

    First version of a simple image publisher

---
+++ devtools/publish-images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+APPS="api apiv1 cli frontend broker message_handler identity_worker identity_poller"
+STAGE=$1
+DOCKER_REPO="registry.caliopen.org"
+REPO_USER=""
+REPO_PASS=""
+VERSION="0.10"
+
+if [[ ${STAGE} =~ ^(develop|master)$ ]]
+then
+	git checkout ${STAGE}
+	for app in ${APPS}; do
+		make ${STAGE} APP_NAME=$app DOCKER_REPO=$DOCKER_REPO REPO_USER=$REPO_USER REPO_PASS=$REPO_PASS VERSION=$VERSION
+	done	
+else
+	echo "Options are develop and master" 
+	exit 1
+fi
