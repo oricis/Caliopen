4403b77b6bac1d706d7681e839927ed836382ddd
Pablo <ppiedrafita95@gmail.com>
2018-06-29 16:44:56 +0200

    Registry variables are read from a separate file

--- devtools/publish-images.sh
+++ devtools/publish-images.sh
@@ -3,16 +3,14 @@ set -e
 
 APPS="api apiv1 cli frontend broker message_handler identity_worker identity_poller"
 STAGE=$1
-DOCKER_REPO="registry.caliopen.org"
-REPO_USER=""
-REPO_PASS=""
 VERSION="${CALIOPEN_VERSION}"
+source ./registry.conf
 
 if [[ ${STAGE} =~ ^(develop|master)$ ]]
 then
 	git checkout ${STAGE}
 	for app in ${APPS}; do
-		make ${STAGE} APP_NAME=$app DOCKER_REPO=$DOCKER_REPO REPO_USER=$REPO_USER REPO_PASS=$REPO_PASS VERSION=$VERSION
+		make ${STAGE} APP_NAME=$app
 	done	
 else
 	echo "Options are develop and master" 
