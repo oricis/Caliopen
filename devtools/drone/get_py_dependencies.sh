ff6b98aca70b8769cfa07df8b9aa4598a5e01e5f
Pablo <ppiedrafita95@gmail.com>
2018-09-04 17:15:22 +0200

    Use correct path to find an app dependencies

--- devtools/drone/get_py_dependencies.sh
+++ devtools/drone/get_py_dependencies.sh
@@ -3,10 +3,10 @@ set -e
 
 DEPS=""
 # Find python requirement file
-REQ_FILE=$(find $PROG -depth -type f -name requirements.deps)
+REQ_FILE=$(find $BASE_DIR/$PROG -depth -type f -name requirements.deps)
 
 # For every line in the file we search the dependency
 while read -r line || [[ -n "$line" ]]; do
-	DEP=`find src/backend -depth -type f,d -name $line | cut -d'/' -f 3- | tr '\n' ' '`
+	DEP=`find $BASE_DIR -depth -type f,d -name $line | cut -d'/' -f 3- | tr '\n' ' '`
 	DEPS="$DEPS $DEP"
 done < $REQ_FILE
