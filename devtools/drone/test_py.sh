387fde2db952e367580d1c658baf01d9a16bf88c
Pablo <ppiedrafita95@gmail.com>
2018-09-13 11:27:35 +0200

    gpg should install before pi [skip CI]

--- devtools/drone/test_py.sh
+++ devtools/drone/test_py.sh
@@ -8,8 +8,8 @@ then
 fi
 
 cd src/backend
-pip install main/py.storage components/py.pi main/py.main interfaces/REST/py.server 
-pip install tools/py.doc interfaces/NATS/py.client tools/py.CLI components/py.pgp tools/py.doc
+pip install main/py.storage componentes/py.gpg components/py.pi main/py.main interfaces/REST/py.server 
+pip install tools/py.doc interfaces/NATS/py.client tools/py.CLI tools/py.doc
 pip install ipdb docker-compose gitpython nose
 nosetests -sv main/py.main/caliopen_main/tests
 nosetests -sv components/py.pi/caliopen_pi/tests
