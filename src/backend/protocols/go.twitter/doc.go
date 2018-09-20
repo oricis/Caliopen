b23d23d8f0cbda0f21c52d9fd8d75478b7f1949d
stan <stan@sapienssapide.fr>
2018-09-20 12:27:24 +0200

    Scaffold a daemon that does nothing more than starting & reading config file.

---
+++ src/backend/protocols/go.twitter/doc.go
@@ -0,0 +1,7 @@
+// Copyleft (ɔ) 2018 The Caliopen contributors.
+// Use of this source code is governed by a GNU AFFERO GENERAL PUBLIC
+// license (AGPL) that can be found in the LICENSE file.
+
+// caliopen_twitterd is a daemon for I/O operations between Caliopen accounts and Twitter accounts
+
+package caliopen_twitterd
