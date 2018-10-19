715cccb363809e270afdc215b06d4c3a7e4604c4
David Epely <esion99@gmail.com>
2018-10-19 12:18:05 +0200

    missing of update frontend unit test related to lingui

--- src/frontend/web_application/test/unit/lingui-react.jsx
+++ src/frontend/web_application/test/unit/lingui-react.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 jest.mock('@lingui/react', () => ({
   withI18n: () => WrappedComponent => props =>
-    (<WrappedComponent i18n={{ _: (id, { defaults }) => (defaults || id) }} {...props} />),
+    (<WrappedComponent i18n={{ _: (id, values, { defaults }) => (defaults || id) }} {...props} />),
   i18nMark: str => str,
   Trans: ({ id, children }) => children || id,
   NumberFormat: ({ value }) => value,
+}));
+
+jest.mock('@lingui/macro', () => ({
+  Trans: ({ id, children }) => {
+    console.log('trans');
+
+    return children || id;
+  },
 }));
