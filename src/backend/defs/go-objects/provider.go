da4e7a0a5ee781e37759641fa7fdb9371c29be89
stan <stan@sapienssapide.fr>
2018-10-19 14:33:49 +0200

    Change the way provider's params are written in config file

--- src/backend/defs/go-objects/provider.go
+++ src/backend/defs/go-objects/provider.go
@@ -1,12 +1,11 @@
 package objects
 
 type Provider struct {
-	ConsumerKey      string `json:"-"                                mapstructure:"consumer_key"`
-	ConsumerSecret   string `json:"-"                                mapstructure:"consumer_secret"`
-	Name             string `json:"name"                             mapstructure:"name"`
-	OauthCallbackUri string `json:"oauth_callback_uri,omitempty"     mapstructure:"oauth_callback_uri"`
-	OauthRequestUrl  string `json:"oauth_request_url,omitempty"      mapstructure:"oauth_request_url"`
-	Protocol         string `json:"protocol"                         mapstructure:"protocol"`
+	Infos            map[string]string `json:"infos"                            mapstructure:"infos"`
+	Name             string            `json:"name"                             mapstructure:"name"`
+	OauthCallbackUri string            `json:"oauth_callback_uri,omitempty"     mapstructure:"oauth_callback_uri"`
+	OauthRequestUrl  string            `json:"oauth_request_url,omitempty"      mapstructure:"oauth_request_url"`
+	Protocol         string            `json:"protocol"                         mapstructure:"protocol"`
 }
 
 type OauthSession struct {
