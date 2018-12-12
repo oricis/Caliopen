1622a99f2e2c0812512628d8099d7216277688fa
stan <stan@sapienssapide.fr>
2018-12-12 16:01:04 +0100

    Share store, index and nats connexions between twitterworkers.

--- src/backend/brokers/go.twitter/broker.go
+++ src/backend/brokers/go.twitter/broker.go
@@ -7,15 +7,11 @@
 package twitter_broker
 
 import (
-	"errors"
 	. "github.com/CaliOpen/Caliopen/src/backend/defs/go-objects"
 	"github.com/CaliOpen/Caliopen/src/backend/main/go.backends"
-	"github.com/CaliOpen/Caliopen/src/backend/main/go.backends/index/elasticsearch"
-	"github.com/CaliOpen/Caliopen/src/backend/main/go.backends/store/cassandra"
 	"github.com/CaliOpen/Caliopen/src/backend/main/go.main/facilities/Notifications"
 	"github.com/CaliOpen/go-twitter/twitter"
 	log "github.com/Sirupsen/logrus"
-	"github.com/gocql/gocql"
 	"github.com/nats-io/go-nats"
 )
 
@@ -67,92 +63,13 @@ type (
 	}
 )
 
-func Initialize(conf BrokerConfig) (broker *TwitterBroker, err error) {
-	var e error
+func Initialize(conf BrokerConfig, store backends.LDAStore, index backends.LDAIndex, natsConn *nats.Conn, notifier *Notifications.Notifier) (broker *TwitterBroker, err error) {
 	broker = new(TwitterBroker)
 	broker.Config = conf
-	switch conf.StoreName {
-	case "cassandra":
-		c := store.CassandraConfig{
-			Hosts:       conf.StoreConfig.Hosts,
-			Keyspace:    conf.StoreConfig.Keyspace,
-			Consistency: gocql.Consistency(conf.StoreConfig.Consistency),
-			SizeLimit:   conf.StoreConfig.SizeLimit,
-			UseVault:    conf.StoreConfig.UseVault,
-		}
-		if conf.StoreConfig.ObjectStore == "s3" {
-			c.WithObjStore = true
-			c.Endpoint = conf.StoreConfig.OSSConfig.Endpoint
-			c.AccessKey = conf.StoreConfig.OSSConfig.AccessKey
-			c.SecretKey = conf.StoreConfig.OSSConfig.SecretKey
-			c.RawMsgBucket = conf.StoreConfig.OSSConfig.Buckets["raw_messages"]
-			c.AttachmentBucket = conf.StoreConfig.OSSConfig.Buckets["temporary_attachments"]
-			c.Location = conf.StoreConfig.OSSConfig.Location
-		}
-		if conf.StoreConfig.UseVault {
-			c.HVaultConfig.Url = conf.StoreConfig.VaultConfig.Url
-			c.HVaultConfig.Username = conf.StoreConfig.VaultConfig.Username
-			c.HVaultConfig.Password = conf.StoreConfig.VaultConfig.Password
-		}
-		b, e := store.InitializeCassandraBackend(c)
-		if e != nil {
-			err = e
-			log.WithError(err).Warnf("[EmailBroker] initialization of %s backend failed", conf.StoreName)
-			return
-		}
-
-		broker.Store = backends.LDAStore(b) // type conversion to LDA interface
-	default:
-		log.Warnf("[TwitterBroker] unknown store backend: %s", conf.StoreName)
-		err = errors.New("[TwitterBroker] unknown store backend")
-		return
-	}
-
-	switch conf.LDAConfig.IndexName {
-	case "elasticsearch":
-		c := index.ElasticSearchConfig{
-			Urls: conf.LDAConfig.IndexConfig.Urls,
-		}
-		i, e := index.InitializeElasticSearchIndex(c)
-		if e != nil {
-			err = e
-			log.WithError(err).Warnf("[EmailBroker] initialization of %s backend failed", conf.IndexName)
-			return
-		}
-
-		broker.Index = backends.LDAIndex(i) // type conversion to LDA interface
-	default:
-		log.Warnf("[TwitterBroker] unknown index backend: %s", conf.LDAConfig.IndexName)
-		err = errors.New("[TwitterBroker] unknown index backend")
-		return
-	}
-
-	broker.NatsConn, e = nats.Connect(conf.NatsURL)
-	if e != nil {
-		err = e
-		log.WithError(err).Warn("[EmailBroker] initalization of NATS connexion failed")
-		return
-	}
-	caliopenConfig := CaliopenConfig{
-		NotifierConfig: conf.LDAConfig.NotifierConfig,
-		NatsConfig: NatsConfig{
-			Url: conf.NatsURL,
-		},
-		RESTstoreConfig: RESTstoreConfig{
-			BackendName:  conf.StoreName,
-			Consistency:  conf.StoreConfig.Consistency,
-			Hosts:        conf.StoreConfig.Hosts,
-			Keyspace:     conf.StoreConfig.Keyspace,
-			OSSConfig:    conf.StoreConfig.OSSConfig,
-			ObjStoreType: conf.StoreConfig.ObjectStore,
-			SizeLimit:    conf.StoreConfig.SizeLimit,
-		},
-		RESTindexConfig: RESTIndexConfig{
-			Hosts:     conf.LDAConfig.IndexConfig.Urls,
-			IndexName: conf.LDAConfig.IndexName,
-		},
-	}
-	broker.Notifier = Notifications.NewNotificationsFacility(caliopenConfig, broker.NatsConn)
+	broker.Store = store
+	broker.Index = index
+	broker.NatsConn = natsConn
+	broker.Notifier = notifier
 	broker.Connectors = TwitterBrokerConnectors{
 		Egress: make(chan NatsCom, 5),
 	}
