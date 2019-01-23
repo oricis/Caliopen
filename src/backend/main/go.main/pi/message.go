020798ca9e4b89238a0f26c1f188965978c34125
Aymeric Barantal <mric@gandi.net>
2019-01-23 10:19:27 +0100

    Use more features for pi.Transport compute

--- src/backend/main/go.main/pi/message.go
+++ src/backend/main/go.main/pi/message.go
@@ -6,7 +6,9 @@ package pi
 
 import (
 	. "github.com/CaliOpen/Caliopen/src/backend/defs/go-objects"
+	log "github.com/Sirupsen/logrus"
 	"math"
+	"strconv"
 	"strings"
 )
 
@@ -14,6 +16,7 @@ import (
 func ComputePIMessage(message *Message) {
 	piMessage := &PIMessage{Transport: 0, Social: 0, Content: 0}
 	features := *message.Privacy_features
+	log.Info("PI: Using features ", features)
 	// pi.Content
 	if encrypted, ok := features["messsage_encryption_method"]; ok && encrypted != "" {
 		piMessage.Content += 50
@@ -28,11 +31,11 @@ func ComputePIMessage(message *Message) {
 	known_participants := 0.0
 	for _, participant := range message.Participants {
 		if len(participant.Contact_ids) > 0 {
-			known_participants += 1.0
+			known_participants += 1
 		}
 	}
 	ratio := known_participants / float64(len(message.Participants)) * 100
-	piMessage.Social += uint32(math.Min(50, ratio))
+	piMessage.Social += uint32(math.Min(80, ratio))
 
 	// pi.Transport
 	trSigned, ok := features["transport_signed"]
@@ -40,5 +43,18 @@ func ComputePIMessage(message *Message) {
 	if ok && len(trSigned) > 0 && strings.ToLower(string(trSigned[0])) == "t" {
 		piMessage.Transport += 20
 	}
+	nbHops, ok := features["nb_external_hops"]
+	if ok {
+		value, err := strconv.Atoi(nbHops)
+		if err != nil && value == 1 {
+			piMessage.Transport += 20
+		}
+	}
+	inCipher, ok := features["ingress_cipher"]
+	if ok && inCipher != "" {
+		piMessage.Transport += 20
+	}
+	// TODO : normalize
+	log.Info("PI: result ", piMessage)
 	message.PI = piMessage
 }
