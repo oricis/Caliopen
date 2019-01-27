3f28d17216bda5902a56cec1e29810a600d1fa87
Aymeric Barantal <mric@gandi.net>
2019-01-27 15:41:50 +0100

    Call setup privacy feature method

--- src/backend/tools/py.CLI/caliopen_cli/commands/setup.py
+++ src/backend/tools/py.CLI/caliopen_cli/commands/setup.py
@@ -4,6 +4,8 @@ import logging
 
 from .setup_storage import setup_storage
 from .setup_notifications_ttls import setup_notifications_ttls
+from .setup_feature import setup_privacy_features
+
 
 log = logging.getLogger(__name__)
 
@@ -14,3 +16,4 @@ def setup():
     setup_storage()
     log.info('Setup notification ttls')
     setup_notifications_ttls()
+    setup_privacy_features()
