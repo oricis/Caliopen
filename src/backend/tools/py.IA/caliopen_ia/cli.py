ef9b23756e19f247003cff55ed690cc4b454901a
Aymeric Barantal <mric@gandi.net>
2019-01-28 16:37:32 +0100

    Start to integrate caliopen_tag ModelManager in CLI

--- src/backend/tools/py.IA/caliopen_ia/cli.py
+++ src/backend/tools/py.IA/caliopen_ia/cli.py
@@ -4,6 +4,9 @@ Command Line Interface (CLI) for caliopen project
 
 """
 import click
+from minio import Minio
+from minio.error import BucketAlreadyOwnedByYou, BucketAlreadyExists
+from caliopen_tag.models_manager import ModelManager, ESDataManager
 
 
 class Config(object):
@@ -14,6 +17,28 @@ class Config(object):
         from caliopen_storage.helpers.connection import connect_storage
         self.conf = Configuration.load(filename, 'global')
         connect_storage()
+
+
+def save_object_store(config, input_file, dest):
+    """Save an input file as dest on object store."""
+    bucket = config['buckets']['learn_models']
+    client = Minio(config["endpoint"],
+                   access_key=config["access_key"],
+                   secret_key=config["secret_key"],
+                   secure=False)
+    try:
+        client.make_bucket(bucket, config['location'])
+    except BucketAlreadyOwnedByYou:
+        pass
+    except BucketAlreadyExists:
+        pass
+    except Exception as exc:
+        raise exc
+    try:
+        resp = minioClient.fput_object(bucket, dest, input_file)
+    except Exception as exc:
+        log.exception('Unable to save file in object store %r' % exc)
+        raise exc
 
 
 @click.group()
@@ -27,8 +52,13 @@ def cli(ctx, config):
 @cli.command()
 @click.argument('model')
 @click.option('--index', default='all')
+@click.option('--output')
 @click.pass_obj
-def train(obj, model, index):
+def train(config, model, index, output):
     """Train a model command."""
     click.echo('Will train model {0} with index {1}'.
                format(model, index))
+    if model == 'tagger':
+        provider = ESDataManager(config)
+        model = ModelManager(provider)
+        model.get_new_model(output)
