7f856c51fc83fafb600ebab666c5acb18364775a
stan S.S <stan@sapienssapide.fr>
2019-02-14 12:31:31 +0100

    Add missing copyleft headers

--- src/backend/workers/go.remoteIDs/jobs_test.go
+++ src/backend/workers/go.remoteIDs/jobs_test.go
@@ -1,3 +1,7 @@
+// Copyleft (ɔ) 2019 The Caliopen contributors.
+// Use of this source code is governed by a GNU AFFERO GENERAL PUBLIC
+// license (AGPL) that can be found in the LICENSE file.
+
 package go_remoteIDs
 
 import (
