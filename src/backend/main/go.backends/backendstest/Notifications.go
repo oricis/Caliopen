6485c41ce1d1ac6f2263c66f55ad700113b3b12c
stan S.S <stan@sapienssapide.fr>
2019-03-08 17:55:10 +0100

    Add Notifications store and index interfaces to backendstest

---
+++ src/backend/main/go.backends/backendstest/Notifications.go
@@ -0,0 +1,41 @@
+// Copyleft (ɔ) 2019 The Caliopen contributors.
+// Use of this source code is governed by a GNU AFFERO GENERAL PUBLIC
+// license (AGPL) that can be found in the LICENSE file.
+
+package backendstest
+
+import (
+	"errors"
+	. "github.com/CaliOpen/Caliopen/src/backend/defs/go-objects"
+	"time"
+)
+
+type NotificationsStore struct{}
+type NotificationsIndex struct{}
+
+func GetNotificationsBackends() (store NotificationsStore, index NotificationsIndex) {
+	return NotificationsStore{}, NotificationsIndex{}
+}
+
+func (ns NotificationsStore) CreateMessage(msg *Message) error {
+	return errors.New("test interface not implemented")
+}
+func (ns NotificationsStore) UserByUsername(username string) (user *User, err error) {
+	return UserByUsername(username)
+}
+func (ns NotificationsStore) RetrieveLocalsIdentities(userId string) (identities []UserIdentity, err error) {
+	return RetrieveLocalsIdentities(userId)
+}
+func (ns NotificationsStore) PutNotificationInQueue(*Notification) error {
+	return errors.New("test interface not implemented")
+}
+func (ns NotificationsStore) RetrieveNotifications(userId string, from, to time.Time) ([]Notification, error) {
+	return nil, errors.New("test interface not implemented")
+}
+func (ns NotificationsStore) DeleteNotifications(userId string, until time.Time) error {
+	return errors.New("test interface not implemented")
+}
+
+func (ni NotificationsIndex) CreateMessage(user *UserInfo, msg *Message) error {
+	return errors.New("test interface not implemented")
+}
