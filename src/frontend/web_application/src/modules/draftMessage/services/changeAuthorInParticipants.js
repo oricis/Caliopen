5fb2eb667210cfe8336b03d48efb5a350ccf32cd
David Epely <esion99@gmail.com>
2019-03-26 14:35:19 +0100

    initialize a reply with participants

---
+++ src/frontend/web_application/src/modules/draftMessage/services/changeAuthorInParticipants.js
@@ -0,0 +1,14 @@
+export const changeAuthorInParticipants = ({ participants, user }) =>
+  participants && participants.reduce((acc, participant) => {
+    if (participant.contact_ids && participant.contact_ids.includes(user.contact.contact_id)) {
+      return [
+        ...acc,
+        { ...participant, type: 'From' },
+      ];
+    }
+
+    return [
+      ...acc,
+      { ...participant, type: 'To' },
+    ];
+  }, []);
