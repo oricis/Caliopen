0ace2ad1b9defff2796de59218d2db95e4d4e244
David Epely <esion99@gmail.com>
2019-03-26 17:20:00 +0100

    refactor getIdentities and identitiesSelector in the module

--- src/frontend/web_application/src/modules/identity/components/WithIdentities/presenter.jsx
+++ src/frontend/web_application/src/modules/identity/components/WithIdentities/presenter.jsx
@@ -4,42 +4,34 @@ import PropTypes from 'prop-types';
 class WithIdentities extends Component {
   static propTypes = {
     render: PropTypes.func.isRequired,
-    remoteIdentities: PropTypes.arrayOf(PropTypes.shape({})),
+    identities: PropTypes.arrayOf(PropTypes.shape({})),
     remoteIsFetching: PropTypes.bool,
-    getRemoteIdentities: PropTypes.func.isRequired,
-    localIdentities: PropTypes.arrayOf(PropTypes.shape({})),
+    getIdentities: PropTypes.func.isRequired,
     localIsFetching: PropTypes.bool,
-    getLocalIdentities: PropTypes.func.isRequired,
   };
   static defaultProps = {
-    remoteIdentities: undefined,
-    localIdentities: undefined,
+    identities: undefined,
     remoteIsFetching: false,
     localIsFetching: false,
   };
 
   componentDidMount() {
     const {
-      remoteIsFetching, getRemoteIdentities,
-      localIsFetching, getLocalIdentities,
+      remoteIsFetching, getIdentities,
+      localIsFetching,
     } = this.props;
 
-    if (!remoteIsFetching) {
-      getRemoteIdentities();
-    }
-
-    if (!localIsFetching) {
-      getLocalIdentities();
+    if (!remoteIsFetching && !localIsFetching) {
+      getIdentities();
     }
   }
 
   render() {
     const {
-      render, remoteIdentities, remoteIsFetching, localIdentities, localIsFetching,
+      render, identities, remoteIsFetching, localIsFetching,
     } = this.props;
 
     const isFetching = (remoteIsFetching || localIsFetching);
-    const identities = [...localIdentities, ...remoteIdentities];
 
     return render({ identities, isFetching });
   }
