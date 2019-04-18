f58e8b0c8c6c07d72fab766e39dfb6a0fb65dfb2
David Epely <esion99@gmail.com>
2019-04-18 10:49:22 +0200

    fix redirect after send
    + the discussion_id is set after the draft has been sent
    + add redirect on compose scene in case the edited message is not a draft anymore

--- src/frontend/web_application/src/scenes/NewDraft/presenter.jsx
+++ src/frontend/web_application/src/scenes/NewDraft/presenter.jsx
@@ -3,41 +3,62 @@ import PropTypes from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
 import { Redirect } from 'react-router-dom';
 import { DraftMessage } from '../../modules/draftMessage';
-import { withPush, withRouteParams } from '../../modules/routing';
+import { withPush } from '../../modules/routing';
 import { withCloseTab } from '../../modules/tab';
 import DraftDiscussion from './components/DraftDiscussion';
 import './style.scss';
 
 @withPush()
-@withRouteParams()
 @withCloseTab()
 class NewDraft extends Component {
   static propTypes = {
+    getMessage: PropTypes.func.isRequired,
     closeTab: PropTypes.func.isRequired,
     push: PropTypes.func.isRequired,
-    routeParams: PropTypes.shape({}).isRequired,
+    messageId: PropTypes.string,
+    message: PropTypes.shape({}),
   };
+
+  static defaultProps = {
+    messageId: undefined,
+    message: undefined,
+  };
+
+  componentDidMount() {
+    const { messageId, getMessage } = this.props;
+
+    if (messageId) {
+      getMessage({ messageId });
+    }
+  }
 
   handleSent = ({ message }) => {
     const { push } = this.props;
+
+    // closeTab();
 
     return push(`/discussions/${message.discussion_id}`);
   }
 
   render() {
-    const { routeParams, closeTab } = this.props;
+    const { messageId, closeTab, message } = this.props;
 
-    if (!routeParams.messageId) {
+    if (!messageId) {
       return <Redirect to={`/messages/${uuidv4()}`} />;
+    }
+
+    if (message && !message.is_draft) {
+      return <Redirect to={`/discussions/${message.discussion_id}`} />;
     }
 
     return (
       <div className="s-new-draft">
         <DraftMessage
           className="s-new-draft__form"
-          key={routeParams.messageId}
-          internalId={routeParams.messageId}
-          messageId={routeParams.messageId}
+          key={messageId}
+          internalId={messageId}
+          messageId={messageId}
+          message={message}
           hasDiscussion={false}
           onDeleteMessageSuccessfull={closeTab}
           onSent={this.handleSent}
@@ -45,7 +66,7 @@ class NewDraft extends Component {
         <DraftDiscussion
           className="s-new-draft__discussion"
           // used in withDraftMessage
-          messageId={routeParams.messageId}
+          messageId={messageId}
         />
       </div>
     );
