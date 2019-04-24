408c78ca6c7b99c8f04a3f4979fd244278a0e05d
stan S.S <stan@sapienssapide.fr>
2019-04-24 15:02:52 +0200

    Fix subject management when creating a reply

--- src/backend/main/py.main/caliopen_main/message/parameters/draft.py
+++ src/backend/main/py.main/caliopen_main/message/parameters/draft.py
@@ -11,9 +11,9 @@ from caliopen_main.participant.parameters import Participant
 from caliopen_main.message.parameters.external_references import \
     ExternalReferences
 from caliopen_storage.exception import NotFound
+from caliopen_main.common.errors import PatchUnprocessable, PatchConflict, \
+    PatchError
 from caliopen_main.message.store import Message as ModelMessage
-
-from caliopen_main.common.errors import PatchUnprocessable
 
 import logging
 
@@ -61,9 +61,20 @@ class Draft(NewInboundMessage):
         # based on current user's selected identity
         from_participant = self._add_from_participant(user)
 
-        if self.parent_id:  # it is a reply, enforce participants
-            self._build_participants_for_reply(user, from_participant)
+        if hasattr(self, 'parent_id') \
+                and self.parent_id is not None \
+                and self.parent_id != "":
+            # it is a reply, enforce participants
+            # and other mandatory properties
+            try:
+                parent_msg = ModelMessage.get(user_id=user.user_id,
+                                              message_id=self.parent_id)
+            except NotFound:
+                raise PatchError(message="parent message not found")
+            self._build_participants_for_reply(parent_msg, from_participant)
             self._update_external_references(user)
+            self._build_subject_for_reply(parent_msg)
+
         elif self.discussion_id != self.hash_participants:
             # participants_hash has changed, update lookups
             Discussion.upsert_lookups_for_participants(user, self.participants)
@@ -113,7 +124,7 @@ class Draft(NewInboundMessage):
 
         return from_participant
 
-    def _build_participants_for_reply(self, user, sender):
+    def _build_participants_for_reply(self, parent_msg, sender):
         """
         Build participants list from message in-reply to.
 
@@ -125,10 +136,9 @@ class Draft(NewInboundMessage):
         """
         if not self.parent_id:
             return
-
-        parent_message = ModelMessage.get(user_id=user.user_id,
-                                          message_id=self.parent_id)
-        for i, participant in enumerate(parent_message["participants"]):
+        # TODO : manage reply to discussion-list
+        # TODO : and to messages that have a `reply-to` header
+        for i, participant in enumerate(parent_msg.participants):
             if re.match("from", participant['type'], re.IGNORECASE):
                 participant["type"] = "To"
                 self.participants.append(participant)
@@ -143,6 +153,31 @@ class Draft(NewInboundMessage):
                         re.match("cc", participant['type'], re.IGNORECASE) or \
                         re.match("bcc", participant['type'], re.IGNORECASE):
                     self.participants.pop(i)
+
+    def _build_subject_for_reply(self, parent_msg):
+        """
+
+        :param user:
+        :return:
+        """
+        # check subject consistency
+        # (https://www.wikiwand.com/en/List_of_email_subject_abbreviations)
+        # for now, we use standard prefix «Re: » (RFC5322#section-3.6.5)
+        p = re.compile(
+            '([\[\(] *)?(RE?S?|FYI|RIF|I|FS|VB|RV|ENC|ODP|PD|YNT|ILT|SV|VS|VL|AW|WG|ΑΠ|ΣΧΕΤ|ΠΡΘ|תגובה|הועבר|主题|转发|FWD?) *([-:;)\]][ :;\])-]*|$)|\]+ *$',
+            re.IGNORECASE)
+        if hasattr(self, 'subject') and self.subject is not None:
+            if p.sub('', self.subject).strip() != p.sub('',
+                                                        parent_msg.subject).strip():
+                raise PatchConflict(message="subject has been changed")
+        else:
+            # no subject property provided :
+            # add subject from context with only one "Re: " prefix
+            self.subject = "Re: " + p.sub('', parent_msg.subject).strip()
+
+            # TODO: prevent modification of protected attributes
+            # below attributes should not be editable by patch:
+            # - tags
 
     def _update_external_references(self, user):
         """
