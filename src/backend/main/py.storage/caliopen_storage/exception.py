fba37a94d7aefe018752c13f9782886ff648ab5c
Aymeric Barantal <mric@gandi.net>
2019-04-25 12:07:27 +0200

    Introduce a DuplicateObject exception for a not generic processing message already imported

--- src/backend/main/py.storage/caliopen_storage/exception.py
+++ src/backend/main/py.storage/caliopen_storage/exception.py
@@ -5,18 +5,18 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 
 class NotFound(Exception):
-
     """Exception when object is not found in store."""
 
     pass
 
 
 class CredentialException(Exception):
+    """Generic exception during user authentication process."""
 
-    """
-    A specialized Exception, raised during an authentication attempt if an
-    error occurs.
-    This can be that no user was found, OR that password do mismatch.
-    """
+    pass
+
+
+class DuplicateObject(Exception):
+    """Exception when an existing object already exists."""
 
     pass
