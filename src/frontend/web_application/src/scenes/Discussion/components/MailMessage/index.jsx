ad12ac3fb5a6e419fbc62e10ea1a0e3aaa5894e3
Lanza <dev@lanza.fr>
2019-05-06 12:42:47 +0200

    Add links using linkify.

--- src/frontend/web_application/src/scenes/Discussion/components/MailMessage/index.jsx
+++ src/frontend/web_application/src/scenes/Discussion/components/MailMessage/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 import { Trans, withI18n } from '@lingui/react';
 import classnames from 'classnames';
+import Linkify from 'linkifyjs/react';
 import { withScrollTarget } from '../../../../modules/scroll';
 import { withPush } from '../../../../modules/routing';
 import { ParticipantLabel } from '../../../../modules/message';
@@ -77,7 +78,7 @@ class MailMessage extends Component {
     }
 
     return (
-      <TextBlock nowrap={false}><pre className="s-mail-message__content">{message.body}</pre></TextBlock>
+      <TextBlock nowrap={false}><Linkify tagName="pre" className="s-mail-message__content">{message.body}</Linkify></TextBlock>
     );
   }
 
