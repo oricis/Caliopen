cebe028422a75d3da2b41da89513afcc24ad7e8b
David Epely <esion99@gmail.com>
2019-06-07 10:35:38 +0200

    tmp: force frontend tests

--- devtools/drone/test_front.sh
+++ devtools/drone/test_front.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ "$SRC_CHANGED" = "false" ];
-then
-	echo "No changes to frontend"
-	return
-fi
+# if [ "$SRC_CHANGED" = "false" ];
+# then
+# 	echo "No changes to frontend"
+# 	return
+# fi
 
 cd src/frontend/web_application
 yarn
