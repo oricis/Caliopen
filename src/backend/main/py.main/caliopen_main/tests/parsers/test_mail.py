d59f9466e1f7ead11d55d1e0bc022ba00d2451bf
Aymeric Barantal <mric@gandi.net>
2019-06-10 13:29:25 +0200

    Test email from package fixtures and also devtools/fixtures/raw_emails directory

--- src/backend/main/py.main/caliopen_main/tests/parsers/test_mail.py
+++ src/backend/main/py.main/caliopen_main/tests/parsers/test_mail.py
@@ -9,21 +9,29 @@ from zope.interface.verify import verifyObject
 from caliopen_storage.config import Configuration
 
 if 'CALIOPEN_BASEDIR' in os.environ:
-    conf_file = '{}/src/backend/configs/caliopen.yaml.template'. \
-                format(os.environ['CALIOPEN_BASEDIR'])
+    basedir = os.environ['CALIOPEN_BASEDIR']
 else:
-    conf_file = '../../../../../configs/caliopen.yaml.template'
+    # Guess something, we shouldn't and expect stricrly an env variable
+    basedir = '../../../../..'
+
+conf_file = '{}/src/backend/configs/caliopen.yaml.template'.format(basedir)
+raw_directory = '{}/devtools/fixtures/raw_emails'.format(basedir)
 
 Configuration.load(conf_file, 'global')
 
-from caliopen_main.common.interfaces import IMessageParser
-from caliopen_main.message.parsers.mail import MailMessage
+from caliopen_main.common.interfaces import IMessageParser  # noqa
+from caliopen_main.message.parsers.mail import MailMessage  # noqa
 
 
-def load_mail(filename):
-    """Read email from fixtures of an user."""
-    # XXX tofix: set fixtures in a more convenient way to not
-    # have dirty hacking on relative path
+def load_raw_mail(filename):
+    """Read email from raw_directory."""
+    with open('{}/{}'.format(raw_directory, filename)) as f:
+        data = f.read()
+    return data
+
+
+def load_mail_relative(filename):
+    """Read email from fixtures of this package."""
     dir_path = os.path.dirname(os.path.realpath(__file__))
     path = '{}/../fixtures/mail'.format(dir_path)
     with open('{}/{}'.format(path, filename)) as f:
@@ -36,7 +44,7 @@ class TestMailFormat(unittest.TestCase):
 
     def test_signed_mail(self):
         """Test parsing of a pgp signed mail."""
-        data = load_mail('pgp_signed_1.eml')
+        data = load_mail_relative('pgp_signed_1.eml')
         mail = MailMessage(data)
         self.assertTrue(verifyObject(IMessageParser, mail))
         self.assertTrue(len(mail.participants) > 1)
@@ -48,7 +56,7 @@ class TestMailFormat(unittest.TestCase):
 
     def test_encrypted_mail(self):
         """Test parsing of a pgp encrypted mail."""
-        data = load_mail('pgp_crypted_1.eml')
+        data = load_mail_relative('pgp_crypted_1.eml')
         mail = MailMessage(data)
         self.assertTrue(verifyObject(IMessageParser, mail))
         self.assertTrue(len(mail.participants) > 1)
@@ -59,12 +67,77 @@ class TestMailFormat(unittest.TestCase):
         self.assertEqual(mail.extra_parameters, expected)
 
     def test_only_png(self):
-        data = load_mail('png.eml')
+        """Test a plain mail having only an image/png."""
+        data = load_mail_relative('png.eml')
         mail = MailMessage(data)
         self.assertEqual(len(mail.attachments), 1)
         self.assertEqual(mail.attachments[0].content_type, 'image/png')
         self.assertEqual(mail.body_html, '')
         self.assertEqual(mail.body_plain, '')
 
-if __name__ == '__main__':
-    unittest.main()
+
+class TestRawMail(unittest.TestCase):
+    """Parse email from devtools/fixtures/raw_emails for particularities"""
+
+    def test_multipart_html(self):
+        data = load_raw_mail('multipart-html')
+        mail = MailMessage(data)
+        self.assertEqual(mail.attachments, [])
+        self.assertTrue(len(mail.body_html) > 0)
+        self.assertTrue(len(mail.body_plain) > 0)
+
+    def test_multipart_signed(self):
+        data = load_raw_mail('multipart-signed')
+        mail = MailMessage(data)
+        self.assertEqual(len(mail.attachments), 1)
+        self.assertEqual(mail.attachments[0].content_type,
+                         'application/pgp-signature')
+        self.assertTrue(len(mail.body_html) == 0)
+        self.assertTrue(len(mail.body_plain) > 0)
+
+    def test_html_inline_image(self):
+        data = load_raw_mail('html-with-inlined-image')
+        mail = MailMessage(data)
+        self.assertEqual(len(mail.attachments), 1)
+        self.assertEqual(mail.attachments[0].content_type, 'image/png')
+        self.assertTrue(len(mail.body_html) > 0)
+        self.assertTrue(len(mail.body_plain) > 0)
+
+    def test_spam_multipart(self):
+        data = load_raw_mail('spam-multipart')
+        mail = MailMessage(data)
+        self.assertEqual(len(mail.attachments), 0)
+        self.assertTrue(len(mail.body_html) > 0)
+        self.assertTrue(len(mail.body_plain) > 0)
+
+    def test_invalid_utf8(self):
+        data = load_raw_mail('invalid-utf-8')
+        mail = MailMessage(data)
+        self.assertEqual(len(mail.attachments), 0)
+        self.assertTrue(len(mail.body_html) > 0)
+        self.assertTrue(len(mail.body_plain) > 0)
+
+    def test_multipart_mixed(self):
+        data = load_raw_mail('multipart-mixed-mailinglist')
+        mail = MailMessage(data)
+        self.assertEqual(len(mail.attachments), 1)
+        self.assertEqual(mail.attachments[0].content_type,
+                         'application/pgp-signature')
+        self.assertEqual(mail.body_html, '')
+        self.assertTrue(len(mail.body_plain) > 0)
+
+    def test_plain_mail_encoded(self):
+        data = load_raw_mail('email-with-iso-8859-1-encoding')
+        mail = MailMessage(data)
+        self.assertEqual(len(mail.attachments), 0)
+        self.assertTrue(len(mail.body_html) == 0)
+        self.assertTrue(len(mail.body_plain) > 0)
+
+    def test_pgp_encrypted(self):
+        data = load_raw_mail('signed-and-encrypted')
+        mail = MailMessage(data)
+        self.assertEqual(len(mail.attachments), 1)
+        self.assertTrue(len(mail.body_html) == 0)
+        self.assertTrue(len(mail.body_plain) > 0)
+        expected = {'encrypted': 'application/pgp-encrypted', 'lists': []}
+        self.assertEqual(mail.extra_parameters, expected)
