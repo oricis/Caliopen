3fce01e37556cf7e6d05754c099a5e3041c6a6c2
stan S.S <stan@sapienssapide.fr>
2019-07-12 12:34:35 +0200

    Add Mastodon container to NewIdentity component

--- src/frontend/web_application/src/scenes/RemoteIdentitySettings/components/NewIdentity/index.jsx
+++ src/frontend/web_application/src/scenes/RemoteIdentitySettings/components/NewIdentity/index.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { Trans } from '@lingui/macro'; // eslint-disable-line import/no-extraneous-dependencies
+import {Trans} from '@lingui/macro'; // eslint-disable-line import/no-extraneous-dependencies
 import {
   Section, Callout, Icon, Title,
 } from '../../../../components';
@@ -39,51 +39,62 @@ class NewIdentity extends Component {
   }
 
   handleChangeRemoteIdentityEmail = async (...params) => {
-    const { onRemoteIdentityChange } = this.props;
+    const {onRemoteIdentityChange} = this.props;
     await onRemoteIdentityChange(...params);
     this.handleToggleFormEmail();
     this.props.onClear();
   }
 
-  handleOAuthDone = () => {
+  handleOAuthDone = (result) => {
+    if (result && result.response) {
+      console.log(result.response)
+    }
     this.props.onClear();
   }
 
   render() {
-    const { providers } = this.props;
+    const {providers} = this.props;
 
     return (
       <Section
-        title={(<Trans id="remote_identity.add_account">Add an external account</Trans>)}
-        titleProps={{ size: 'large' }}
+        title={(<Trans id="remote_identity.add_account">Add an external
+          account</Trans>)}
+        titleProps={{size: 'large'}}
         className="m-new-identity"
         shape="none"
       >
         <p className="m-new-identity__main-help">
           <Trans id="remote_identity.create_help">
-            Add an external account will allow you to aggregate messages for this account in your
-            Caliopen inbox and answer to your contacts using one of these acounts.
+            Add an external account will allow you to aggregate messages for
+            this account in your
+            Caliopen inbox and answer to your contacts using one of these
+            acounts.
           </Trans>
         </p>
         <Callout color="info">
           <Trans id="remote_identity.how_to">
             <p>
               External accounts are fetched every 15 minutes.
-              <br />
-              Currently there is no indicator to inform that the account is correctly configured
+              <br/>
+              Currently there is no indicator to inform that the account is
+              correctly configured
               until first try is done.
             </p>
           </Trans>
         </Callout>
         <Title hr>
-          <Trans id="remote_identity.choose-provider.title">Select a provider</Trans>
+          <Trans id="remote_identity.choose-provider.title">Select a
+            provider</Trans>
         </Title>
         <ul className="m-provider-list">
           {
             providers.includes(PROVIDER_GMAIL) && (
               <li className="m-provider-list__provider">
-                <ProviderButtonContainer label={(<Trans id="remote_identity.gmail.help">You will be redirected to gmail authentication</Trans>)}>
-                  <AuthButton onDone={this.handleOAuthDone} providerName={PROVIDER_GMAIL} />
+                <ProviderButtonContainer label={(
+                  <Trans id="remote_identity.gmail.help">You will be redirected
+                    to gmail authentication</Trans>)}>
+                  <AuthButton onDone={this.handleOAuthDone}
+                              providerName={PROVIDER_GMAIL}/>
                 </ProviderButtonContainer>
               </li>
             )
@@ -91,17 +102,38 @@ class NewIdentity extends Component {
           {
             providers.includes(PROVIDER_TWITTER) && (
               <li className="m-provider-list__provider">
-                <ProviderButtonContainer label={(<Trans id="remote_identity.twitter.help">You will be redirected to twitter authentication and authorize the application</Trans>)}>
-                  <AuthButton onDone={this.handleOAuthDone} providerName={PROVIDER_TWITTER} />
+                <ProviderButtonContainer label={(
+                  <Trans id="remote_identity.twitter.help">You will be
+                    redirected to twitter authentication and authorize the
+                    application</Trans>)}>
+                  <AuthButton onDone={this.handleOAuthDone}
+                              providerName={PROVIDER_TWITTER}/>
+                </ProviderButtonContainer>
+              </li>
+            )
+          }
+          {
+            providers.includes(PROVIDER_MASTODON) && (
+              <li className="m-provider-list__provider">
+                <ProviderButtonContainer label={(
+                  <Trans id="remote_identity.mastodon.help">You will be
+                    redirected to mastodon authentication and authorize the
+                    application</Trans>)}>
+                  <AuthButton onDone={this.handleOAuthDone}
+                              providerName={PROVIDER_MASTODON}/>
                 </ProviderButtonContainer>
               </li>
             )
           }
           <li className="m-provider-list__provider">
-            <ProviderButtonContainer label={(<Trans id="remote_identity.email.help">You will have to fill the form with your imap server&apos;s parameters</Trans>)}>
+            <ProviderButtonContainer label={(
+              <Trans id="remote_identity.email.help">You will have to fill the
+                form with your imap server&apos;s parameters</Trans>)}>
               <ProviderButton onClick={this.handleToggleFormEmail}>
-                <Icon className="m-provider-email-button__icon" type="email" rightSpaced />
-                <Trans id="remote_identity.action.toggle-email-form">Email (via IMAP)</Trans>
+                <Icon className="m-provider-email-button__icon" type="email"
+                      rightSpaced/>
+                <Trans id="remote_identity.action.toggle-email-form">Email (via
+                  IMAP)</Trans>
               </ProviderButton>
             </ProviderButtonContainer>
           </li>
