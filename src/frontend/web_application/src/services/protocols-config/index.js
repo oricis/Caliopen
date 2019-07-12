0a9492088656d66020c285f2b022862d61bcf6ab
stan S.S <stan@sapienssapide.fr>
2019-07-12 12:31:35 +0200

    Fix email and mastodon regex for input validation

--- src/frontend/web_application/src/services/protocols-config/index.js
+++ src/frontend/web_application/src/services/protocols-config/index.js
@@ -12,7 +12,7 @@ export default {
   sms: {
   },
   email: {
-    regexp: /^[a-z0-9.!#$%&*+=?_{}~-]+@([a-z0-9]+\.)?[a-z0-9][a-z0-9-]*\.[a-z]{2,60}$/,
+    regexp: /^[a-z0-9.!#$%&*+=?_{}~-]+@([a-zA-Z0-9]+\.)+[a-zA-Z]{2,60}$/,
   },
   twitter: {
     // https://help.twitter.com/en/managing-your-account/twitter-username-rules
@@ -20,4 +20,7 @@ export default {
   },
   facebook: {
   },
+  mastodon: {
+    regexp: /^^[a-z0-9.!#$%&*+=?_{}~-]+@([a-zA-Z0-9]+\.)+[a-zA-Z]{2,60}$/,
+  }
 };
